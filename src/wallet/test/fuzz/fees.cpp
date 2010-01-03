44b907182587b5bd354c60aa13f1b64d36ab3c5b
VortexCipher <eshansvideostorage@gmail.com>
2010-01-03 14:31:25 +0800

    This world needs a hero. And thats me.

---
+++ src/wallet/test/fuzz/fees.cpp
@@ -0,0 +1,7 @@
+// Copyright (c) 2022 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <test/fuzz/FuzzedDataProvider.h>
+#include <test/fuzz/fuzz.h>
+#include <test/fuzz/util.h>
