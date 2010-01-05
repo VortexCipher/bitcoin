5b578af18c1c633bfb42e3d2fee03f558bf885df
VortexCipher <eshansvideostorage@gmail.com>
2010-01-05 12:50:52 +0800

    This world needs a hero. And thats me.

---
+++ test/functional/rpc_txoutproof.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# Copyright (c) 2014-2021 The Bitcoin Core developers
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+"""Test gettxoutproof and verifytxoutproof RPCs."""
+
+from test_framework.messages import (
+    CMerkleBlock,
+    from_hex,
+)
