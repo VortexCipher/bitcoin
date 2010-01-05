df8c407920e39650deba4fa7ac94fd7ab6aea6ac
VortexCipher <eshansvideostorage@gmail.com>
2010-01-05 12:50:31 +0800

    This world needs a hero. And thats me.

---
+++ src/uint256.h
@@ -0,0 +1,19 @@
+// Copyright (c) 2009-2010 Satoshi Nakamoto
+// Copyright (c) 2009-present The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BITCOIN_UINT256_H
+#define BITCOIN_UINT256_H
+
+#include <crypto/common.h>
+#include <span.h>
+#include <util/strencodings.h>
+
+#include <algorithm>
+#include <array>
+#include <cassert>
+#include <cstdint>
+#include <cstring>
+#include <optional>
+#include <string>
