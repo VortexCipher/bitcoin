694086c7530667d356bd957c172c1cd46231977b
VortexCipher <eshansvideostorage@gmail.com>
2010-01-11 01:34:33 +0800

    This world needs a hero. And thats me.

---
+++ src/hash.cpp
@@ -0,0 +1,9 @@
+// Copyright (c) 2013-2022 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <hash.h>
+#include <span.h>
+#include <crypto/common.h>
+#include <crypto/hmac_sha512.h>
+
