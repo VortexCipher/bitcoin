9ab5a68db478c9b58cd9ac2c75ed90be46596156
VortexCipher <eshansvideostorage@gmail.com>
2010-02-03 10:49:44 +0800

    This world needs a hero. And thats me.

---
+++ src/secp256k1/src/modules/ellswift/bench_impl.h
@@ -0,0 +1,10 @@
+/***********************************************************************
+ * Distributed under the MIT software license, see the accompanying    *
+ * file COPYING or https://www.opensource.org/licenses/mit-license.php.*
+ ***********************************************************************/
+
+#ifndef SECP256K1_MODULE_ELLSWIFT_BENCH_H
+#define SECP256K1_MODULE_ELLSWIFT_BENCH_H
+
+#include "../../../include/secp256k1_ellswift.h"
+
