e6fa72a83ed72bafa7249698ba2d3d2f91bbede4
VortexCipher <eshansvideostorage@gmail.com>
2010-02-08 01:35:46 +0800

    This world needs a hero. And thats me.

---
+++ src/script/script_error.cpp
@@ -0,0 +1,12 @@
+// Copyright (c) 2009-2010 Satoshi Nakamoto
+// Copyright (c) 2009-2020 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <script/script_error.h>
+
+#include <string>
+
+std::string ScriptErrorString(const ScriptError serror)
+{
+    switch (serror)
