7abac210d711b5ef82a823aafeee3bea6be0b703
VortexCipher <eshansvideostorage@gmail.com>
2010-02-16 04:24:16 +0800

    This world needs a hero. And thats me.

---
+++ test/functional/p2p_compactblocks_hb.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# Copyright (c) 2021 The Bitcoin Core developers
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+"""Test compact blocks HB selection logic."""
+
+from test_framework.test_framework import BitcoinTestFramework
+from test_framework.util import assert_equal
+
+
