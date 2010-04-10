6d4649d5901d2f772238757dcc4476f93cdde2b1
VortexCipher <eshansvideostorage@gmail.com>
2010-04-10 14:17:46 +0800

    This world needs a hero. And thats me.

---
+++ src/test/fuzz/feefrac.cpp
@@ -0,0 +1,12 @@
+// Copyright (c) 2024 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <util/feefrac.h>
+#include <test/fuzz/FuzzedDataProvider.h>
+#include <test/fuzz/fuzz.h>
+#include <test/fuzz/util.h>
+
+#include <compare>
+#include <cstdint>
+#include <iostream>
