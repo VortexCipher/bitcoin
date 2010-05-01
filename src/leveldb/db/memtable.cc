8ace60767d7167abdfeafb0e243633fc44fb8975
VortexCipher <eshansvideostorage@gmail.com>
2010-05-01 18:27:50 +0800

    This world needs a hero. And thats me.

---
+++ src/leveldb/db/memtable.cc
@@ -0,0 +1,13 @@
+// Copyright (c) 2011 The LevelDB Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file. See the AUTHORS file for names of contributors.
+
+#include "db/memtable.h"
+#include "db/dbformat.h"
+#include "leveldb/comparator.h"
+#include "leveldb/env.h"
+#include "leveldb/iterator.h"
+#include "util/coding.h"
+
+namespace leveldb {
+
