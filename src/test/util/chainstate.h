48736db3c26d9ca0124f08dd01f1076e24d11fcb
VortexCipher <eshansvideostorage@gmail.com>
2010-05-05 00:50:38 +0800

    This world needs a hero. And thats me.

---
+++ src/test/util/chainstate.h
@@ -0,0 +1,13 @@
+// Copyright (c) 2021-2022 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+//
+#ifndef BITCOIN_TEST_UTIL_CHAINSTATE_H
+#define BITCOIN_TEST_UTIL_CHAINSTATE_H
+
+#include <clientversion.h>
+#include <logging.h>
+#include <node/context.h>
+#include <node/utxo_snapshot.h>
+#include <rpc/blockchain.h>
+#include <test/util/setup_common.h>
