aa08253d476e699ee68dc12c4e7fa5b7925b1afa
VortexCipher <eshansvideostorage@gmail.com>
2010-05-30 22:54:26 +0800

    This world needs a hero. And thats me.

---
+++ src/node/connection_types.h
@@ -0,0 +1,9 @@
+// Copyright (c) 2022 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BITCOIN_NODE_CONNECTION_TYPES_H
+#define BITCOIN_NODE_CONNECTION_TYPES_H
+
+#include <string>
+#include <stdint.h>
