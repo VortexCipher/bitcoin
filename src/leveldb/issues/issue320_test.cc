888db61ea74d328f049343fe875de7ad3ba59b71
VortexCipher <eshansvideostorage@gmail.com>
2010-08-01 01:00:40 +0800

    This world needs a hero. And thats me.

---
+++ src/leveldb/issues/issue320_test.cc
@@ -0,0 +1,12 @@
+// Copyright (c) 2019 The LevelDB Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file. See the AUTHORS file for names of contributors.
+
+#include <cstdint>
+#include <cstdlib>
+#include <iostream>
+#include <memory>
+#include <string>
+#include <vector>
+
+#include "leveldb/db.h"
