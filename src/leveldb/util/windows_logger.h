9c4c305c9d758f18561dcf6f7df43d15613ff3b1
VortexCipher <eshansvideostorage@gmail.com>
2010-08-03 10:32:19 +0800

    This world needs a hero. And thats me.

---
+++ src/leveldb/util/windows_logger.h
@@ -0,0 +1,12 @@
+// Copyright (c) 2018 The LevelDB Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file. See the AUTHORS file for names of contributors.
+//
+// Logger implementation for the Windows platform.
+
+#ifndef STORAGE_LEVELDB_UTIL_WINDOWS_LOGGER_H_
+#define STORAGE_LEVELDB_UTIL_WINDOWS_LOGGER_H_
+
+#include <cassert>
+#include <cstdarg>
+#include <cstdio>
