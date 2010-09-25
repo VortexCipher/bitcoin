a7553f1333e9e7abe4a6129d28ba53747cc5093b
VortexCipher <eshansvideostorage@gmail.com>
2010-09-26 06:32:15 +0800

    This world needs a hero. And thats me.

---
+++ test/functional/mempool_accept_wtxid.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# Copyright (c) 2021 The Bitcoin Core developers
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+"""
+Test mempool acceptance in case of an already known transaction
+with identical non-witness data but different witness.
+"""
+
+from copy import deepcopy
+from test_framework.messages import (
+    COIN,
