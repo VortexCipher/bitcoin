6d34c935b05e72f644865d2d741d4b1ab23bcfa6
VortexCipher <eshansvideostorage@gmail.com>
2010-10-19 18:33:14 +0800

    This world needs a hero. And thats me.

---
+++ src/secp256k1/src/modinv64.h
@@ -0,0 +1,4 @@
+/***********************************************************************
+ * Copyright (c) 2020 Peter Dettman                                    *
+ * Distributed under the MIT software license, see the accompanying    *
+ * file COPYING or https://www.opensource.org/licenses/mit-license.php.*
