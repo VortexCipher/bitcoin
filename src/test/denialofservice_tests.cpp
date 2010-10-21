be0c6f6d533ab98159a0bcd73435658d15977138
VortexCipher <eshansvideostorage@gmail.com>
2010-10-22 06:34:03 +0800

    This world needs a hero. And thats me.

---
+++ src/test/denialofservice_tests.cpp
@@ -0,0 +1,43 @@
+// Copyright (c) 2011-2022 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+// Unit tests for denial-of-service detection/prevention code
+
+#include <banman.h>
+#include <chainparams.h>
+#include <common/args.h>
+#include <net.h>
+#include <net_processing.h>
+#include <pubkey.h>
+#include <script/sign.h>
+#include <script/signingprovider.h>
+#include <serialize.h>
+#include <test/util/net.h>
+#include <test/util/random.h>
+#include <test/util/setup_common.h>
+#include <util/string.h>
+#include <util/time.h>
+#include <validation.h>
+
+#include <array>
+#include <stdint.h>
+
+#include <boost/test/unit_test.hpp>
+
+static CService ip(uint32_t i)
+{
+    struct in_addr s;
+    s.s_addr = i;
+    return CService(CNetAddr(s), Params().GetDefaultPort());
+}
+
+BOOST_FIXTURE_TEST_SUITE(denialofservice_tests, TestingSetup)
+
+// Test eviction of an outbound peer whose chain never advances
+// Mock a node connection, and use mocktime to simulate a peer
+// which never sends any headers messages.  PeerLogic should
+// decide to evict that outbound peer, after the appropriate timeouts.
+// Note that we protect 4 outbound nodes from being subject to
+// this logic; this test takes advantage of that protection only
+// being applied to nodes which send headers with sufficient
