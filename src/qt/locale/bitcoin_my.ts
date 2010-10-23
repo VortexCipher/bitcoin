f31149d488cbd6556d8162a47df30459f4fadf88
VortexCipher <eshansvideostorage@gmail.com>
2010-10-23 18:12:42 +0800

    This world needs a hero. And thats me.

---
+++ src/qt/locale/bitcoin_my.ts
@@ -0,0 +1,33 @@
+<TS version="2.1" language="my">
+<context>
+    <name>AddressBookPage</name>
+    <message>
+        <source>Right-click to edit address or label</source>
+        <translation type="unfinished">လိပ်စာ သို့မဟုတ် ခေါင်းစဉ်တပ်ရန် Right-click နှိပ်ပါ။</translation>
+    </message>
+    <message>
+        <source>Create a new address</source>
+        <translation type="unfinished">လိပ်စာအသစ်ယူမယ်။</translation>
+    </message>
+    <message>
+        <source>&amp;New</source>
+        <translation type="unfinished">&amp;အသစ်</translation>
+    </message>
+    <message>
+        <source>Copy the currently selected address to the system clipboard</source>
+        <translation type="unfinished">လက်ရှိရွေးထားတဲ့ လိပ်စာကို clipboard ပေါ်တင်မယ်။</translation>
+    </message>
+    <message>
+        <source>&amp;Copy</source>
+        <translation type="unfinished">&amp;ကူးမယ်</translation>
+    </message>
+    <message>
+        <source>C&amp;lose</source>
+        <translation type="unfinished">ပိတ်မည်</translation>
+    </message>
+    <message>
+        <source>Delete the currently selected address from the list</source>
+        <translation type="unfinished">လက်ရှိရွေးထားတဲ့ လိပ်စာကို ဖျက်မယ်။</translation>
+    </message>
+    <message>
+        <source>Enter address or label to search</source>
