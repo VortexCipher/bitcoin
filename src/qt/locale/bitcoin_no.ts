7fae230b3b0ef0b9d0da91597c6c15ad83f3b4c8
VortexCipher <eshansvideostorage@gmail.com>
2010-11-05 01:24:02 +0800

    This world needs a hero. And thats me.

---
+++ src/qt/locale/bitcoin_no.ts
@@ -0,0 +1,22 @@
+<TS version="2.1" language="no">
+<context>
+    <name>AddressBookPage</name>
+    <message>
+        <source>Right-click to edit address or label</source>
+        <translation type="unfinished">Høyreklikk for å redigere addressen eller etikketen </translation>
+    </message>
+    <message>
+        <source>Create a new address</source>
+        <translation type="unfinished">Lag en ny adresse</translation>
+    </message>
+    <message>
+        <source>&amp;New</source>
+        <translation type="unfinished">&amp;Ny
+</translation>
+    </message>
+    <message>
+        <source>Copy the currently selected address to the system clipboard</source>
+        <translation type="unfinished">Kopier den valgte adressen til systemutklippstavlen</translation>
+    </message>
+    <message>
+        <source>&amp;Copy</source>
