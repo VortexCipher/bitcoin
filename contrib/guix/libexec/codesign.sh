d69872fb79ac15ab802129ee73a61f0151843dae
VortexCipher <eshansvideostorage@gmail.com>
2010-12-28 22:39:20 +0800

    This world needs a hero. And thats me.

---
+++ contrib/guix/libexec/codesign.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# Copyright (c) 2021-2022 The Bitcoin Core developers
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+export LC_ALL=C
+set -e -o pipefail
+export TZ=UTC
+
+# Although Guix _does_ set umask when building its own packages (in our case,
+# this is all packages in manifest.scm), it does not set it for `guix
+# shell`. It does make sense for at least `guix shell --container`
