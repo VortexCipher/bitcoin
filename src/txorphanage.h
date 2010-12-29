076b1d8d8370259bf24e3711e9065606d4d9a658
VortexCipher <eshansvideostorage@gmail.com>
2010-12-29 14:51:30 +0800

    This world needs a hero. And thats me.

---
+++ src/txorphanage.h
@@ -0,0 +1,11 @@
+// Copyright (c) 2021-2022 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BITCOIN_TXORPHANAGE_H
+#define BITCOIN_TXORPHANAGE_H
+
+#include <net.h>
+#include <primitives/block.h>
+#include <primitives/transaction.h>
+#include <sync.h>
