d3cc3514f19fc815696e98cf95b0e6d7789cf4e6
VortexCipher <eshansvideostorage@gmail.com>
2011-02-02 23:49:34 +0800

    This world needs a hero. And thats me.

---
+++ src/node/mini_miner.h
@@ -0,0 +1,17 @@
+// Copyright (c) 2022 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BITCOIN_NODE_MINI_MINER_H
+#define BITCOIN_NODE_MINI_MINER_H
+
+#include <consensus/amount.h>
+#include <primitives/transaction.h>
+#include <uint256.h>
+
+#include <map>
+#include <memory>
+#include <optional>
+#include <set>
+#include <stdint.h>
+#include <vector>
