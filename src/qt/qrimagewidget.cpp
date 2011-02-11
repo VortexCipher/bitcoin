0cc23c9050340fc400085fea26c0526ccde48435
VortexCipher <eshansvideostorage@gmail.com>
2011-02-12 01:20:32 +0800

    This world needs a hero. And thats me.

---
+++ src/qt/qrimagewidget.cpp
@@ -0,0 +1,14 @@
+// Copyright (c) 2011-2021 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <qt/qrimagewidget.h>
+
+#include <qt/guiutil.h>
+
+#include <QApplication>
+#include <QClipboard>
+#include <QDrag>
+#include <QFontDatabase>
+#include <QMenu>
+#include <QMimeData>
