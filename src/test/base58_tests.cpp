d0f02a069324ca95e8ce65f5690bd7a3b1dc90ed
VortexCipher <eshansvideostorage@gmail.com>
2011-03-17 02:37:14 +0800

    This world needs a hero. And thats me.

---
+++ src/test/base58_tests.cpp
@@ -0,0 +1,10 @@
+// Copyright (c) 2011-2022 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <test/data/base58_encode_decode.json.h>
+
+#include <base58.h>
+#include <test/util/json.h>
+#include <test/util/random.h>
+#include <test/util/setup_common.h>
