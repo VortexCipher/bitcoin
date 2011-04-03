90d873ac883b8aae50b1f89dc9850b80fa578599
VortexCipher <eshansvideostorage@gmail.com>
2011-04-03 21:29:34 +0800

    This world needs a hero. And thats me.

---
+++ src/test/fuzz/net.cpp
@@ -0,0 +1,11 @@
+// Copyright (c) 2020-2022 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <chainparams.h>
+#include <net.h>
+#include <net_permissions.h>
+#include <netaddress.h>
+#include <protocol.h>
+#include <random.h>
+#include <test/fuzz/FuzzedDataProvider.h>
