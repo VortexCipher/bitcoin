e8a2b76ad171866554298a77875ef34893082a0b
VortexCipher <eshansvideostorage@gmail.com>
2011-04-22 04:23:09 +0800

    This world needs a hero. And thats me.

---
+++ src/util/feefrac.cpp
@@ -0,0 +1,7 @@
+// Copyright (c) The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <util/feefrac.h>
+#include <algorithm>
+#include <array>
