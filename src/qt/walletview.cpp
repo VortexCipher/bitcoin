2d8376cbd23d051b872bcf0b46673db4a307bc7f
VortexCipher <eshansvideostorage@gmail.com>
2011-04-25 05:05:55 +0800

    This world needs a hero. And thats me.

---
+++ src/qt/walletview.cpp
@@ -0,0 +1,28 @@
+// Copyright (c) 2011-2022 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <qt/walletview.h>
+
+#include <qt/addressbookpage.h>
+#include <qt/askpassphrasedialog.h>
+#include <qt/clientmodel.h>
+#include <qt/guiutil.h>
+#include <qt/optionsmodel.h>
+#include <qt/overviewpage.h>
+#include <qt/platformstyle.h>
+#include <qt/receivecoinsdialog.h>
+#include <qt/sendcoinsdialog.h>
+#include <qt/signverifymessagedialog.h>
+#include <qt/transactiontablemodel.h>
+#include <qt/transactionview.h>
+#include <qt/walletmodel.h>
+
+#include <interfaces/node.h>
+#include <node/interface_ui.h>
+#include <util/strencodings.h>
+
+#include <QAction>
+#include <QFileDialog>
+#include <QHBoxLayout>
+#include <QProgressDialog>
