6d3b4e046780e5cf9804b7cd73e1e8487dda14ef
VortexCipher <eshansvideostorage@gmail.com>
2011-05-11 00:31:03 +0800

    This world needs a hero. And thats me.

---
+++ test/functional/rpc_misc.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# Copyright (c) 2019-2022 The Bitcoin Core developers
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+"""Test RPC misc output."""
+import xml.etree.ElementTree as ET
+
+from test_framework.test_framework import BitcoinTestFramework
+from test_framework.util import (
+    assert_raises_rpc_error,
