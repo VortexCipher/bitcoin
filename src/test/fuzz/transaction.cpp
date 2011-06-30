b4661b69e964bf863256f56dfbfd38851424ebac
VortexCipher <eshansvideostorage@gmail.com>
2011-06-30 13:01:50 +0800

    This world needs a hero. And thats me.

---
+++ src/test/fuzz/transaction.cpp
@@ -0,0 +1,10 @@
+// Copyright (c) 2019-2022 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <chainparams.h>
+#include <coins.h>
+#include <consensus/tx_check.h>
+#include <consensus/tx_verify.h>
+#include <consensus/validation.h>
+#include <core_io.h>
