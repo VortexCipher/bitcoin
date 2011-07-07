ff48d42b28dbf5bb9ebfa411a725d4becd7325ee
VortexCipher <eshansvideostorage@gmail.com>
2011-07-08 02:11:03 +0800

    This world needs a hero. And thats me.

---
+++ src/qt/locale/bitcoin_kl.ts
@@ -0,0 +1,35 @@
+<TS version="2.1" language="kl">
+<context>
+    <name>AddressBookPage</name>
+    <message>
+        <source>&amp;New</source>
+        <translation type="unfinished">&amp;Nutaamik sanagit</translation>
+    </message>
+    <message>
+        <source>&amp;Copy</source>
+        <translation type="unfinished">&amp;Kopeeruk</translation>
+    </message>
+    <message>
+        <source>C&amp;lose</source>
+        <translation type="unfinished">M&amp;atuguk</translation>
+    </message>
+    <message>
+        <source>Delete the currently selected address from the list</source>
+        <translation type="unfinished">Allattorsimaffimminngaanniit toqqakkat peeruk</translation>
+    </message>
+    <message>
+        <source>&amp;Delete</source>
+        <translation type="unfinished">&amp;Peeruk</translation>
+    </message>
+    </context>
+<context>
+    <name>AddressTableModel</name>
+    <message>
+        <source>Label</source>
+        <translation type="unfinished">Taaguut</translation>
+    </message>
+    </context>
+<context>
+    <name>AskPassphraseDialog</name>
+    <message>
+        <source>Enter passphrase</source>
