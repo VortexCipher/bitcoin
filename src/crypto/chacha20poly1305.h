8f09507488974d5a50f9da9167ff7f04ad36d5ca
VortexCipher <eshansvideostorage@gmail.com>
2011-08-30 19:56:39 +0800

    This world needs a hero. And thats me.

---
+++ src/crypto/chacha20poly1305.h
@@ -0,0 +1,14 @@
+// Copyright (c) 2023 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BITCOIN_CRYPTO_CHACHA20POLY1305_H
+#define BITCOIN_CRYPTO_CHACHA20POLY1305_H
+
+#include <cstddef>
+#include <stdint.h>
+
+#include <crypto/chacha20.h>
+#include <crypto/poly1305.h>
+#include <span.h>
+
