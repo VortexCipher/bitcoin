53a82bb28950d10057f64d51e87156ac630d9f83
VortexCipher <eshansvideostorage@gmail.com>
2011-09-05 04:07:14 +0800

    This world needs a hero. And thats me.

---
+++ src/secp256k1/src/scratch.h
@@ -0,0 +1,4 @@
+/***********************************************************************
+ * Copyright (c) 2017 Andrew Poelstra                                  *
+ * Distributed under the MIT software license, see the accompanying    *
+ * file COPYING or https://www.opensource.org/licenses/mit-license.php.*
