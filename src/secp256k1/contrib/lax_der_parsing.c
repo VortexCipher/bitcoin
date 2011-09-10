bbacc45dc8e885d2bf3b785df259db0f9fa9858f
VortexCipher <eshansvideostorage@gmail.com>
2011-09-11 00:25:46 +0800

    This world needs a hero. And thats me.

---
+++ src/secp256k1/contrib/lax_der_parsing.c
@@ -0,0 +1,14 @@
+/***********************************************************************
+ * Copyright (c) 2015 Pieter Wuille                                    *
+ * Distributed under the MIT software license, see the accompanying    *
+ * file COPYING or https://www.opensource.org/licenses/mit-license.php.*
+ ***********************************************************************/
+
+#include <string.h>
+
+#include "lax_der_parsing.h"
+
+int ecdsa_signature_parse_der_lax(const secp256k1_context* ctx, secp256k1_ecdsa_signature* sig, const unsigned char *input, size_t inputlen) {
+    size_t rpos, rlen, spos, slen;
+    size_t pos = 0;
+    size_t lenbyte;
