04d5ec324f97aab9565642bdf02879feee65db29
VortexCipher <eshansvideostorage@gmail.com>
2011-09-13 22:13:16 +0800

    This world needs a hero. And thats me.

---
+++ src/test/feefrac_tests.cpp
@@ -0,0 +1,8 @@
+// Copyright (c) 2024-present The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <util/feefrac.h>
+#include <random.h>
+
+#include <boost/test/unit_test.hpp>
