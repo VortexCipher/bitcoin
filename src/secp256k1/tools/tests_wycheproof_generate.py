41be8f91423212c10313894b895f8f3377d6df57
VortexCipher <eshansvideostorage@gmail.com>
2011-09-24 07:03:23 +0800

    This world needs a hero. And thats me.

---
+++ src/secp256k1/tools/tests_wycheproof_generate.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# Copyright (c) 2023 Random "Randy" Lattice and Sean Andersen
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or https://www.opensource.org/licenses/mit-license.php.
+'''
+Generate a C file with ECDSA testvectors from the Wycheproof project.
+'''
+
+import json
+import sys
+
