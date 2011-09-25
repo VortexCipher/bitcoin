9508e7c83d8a6f47ffdf23355a9ced6b57433230
VortexCipher <eshansvideostorage@gmail.com>
2011-09-26 04:10:59 +0800

    This world needs a hero. And thats me.

---
+++ src/secp256k1/src/modules/ecdh/bench_impl.h
@@ -0,0 +1,5 @@
+/***********************************************************************
+ * Copyright (c) 2015 Pieter Wuille, Andrew Poelstra                   *
+ * Distributed under the MIT software license, see the accompanying    *
+ * file COPYING or https://www.opensource.org/licenses/mit-license.php.*
+ ***********************************************************************/
