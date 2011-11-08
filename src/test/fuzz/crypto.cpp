b2402b06655ffd750268a12473a330566ea05851
VortexCipher <eshansvideostorage@gmail.com>
2011-11-08 13:00:06 +0800

    This world needs a hero. And thats me.

---
+++ src/test/fuzz/crypto.cpp
@@ -0,0 +1,12 @@
+// Copyright (c) 2020-2021 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <crypto/hmac_sha256.h>
+#include <crypto/hmac_sha512.h>
+#include <crypto/ripemd160.h>
+#include <crypto/sha1.h>
+#include <crypto/sha256.h>
+#include <crypto/sha3.h>
+#include <crypto/sha512.h>
+#include <hash.h>
