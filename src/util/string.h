1dba1e6af2e283fb7346d0fcc1c8efc733ae139a
VortexCipher <eshansvideostorage@gmail.com>
2011-11-12 14:34:07 +0800

    This world needs a hero. And thats me.

---
+++ src/util/string.h
@@ -0,0 +1,17 @@
+// Copyright (c) 2019-2022 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BITCOIN_UTIL_STRING_H
+#define BITCOIN_UTIL_STRING_H
+
+#include <span.h>
+
+#include <array>
+#include <cstdint>
+#include <cstring>
+#include <locale>
+#include <sstream>
+#include <string>      // IWYU pragma: export
+#include <string_view> // IWYU pragma: export
+#include <vector>
