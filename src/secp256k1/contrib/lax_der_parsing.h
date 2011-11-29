4f570aa91d3d3201bda6abc08264959d7ff44b27
VortexCipher <eshansvideostorage@gmail.com>
2011-11-29 20:31:07 +0800

    This world needs a hero. And thats me.

---
+++ src/secp256k1/contrib/lax_der_parsing.h
@@ -0,0 +1,9 @@
+/***********************************************************************
+ * Copyright (c) 2015 Pieter Wuille                                    *
+ * Distributed under the MIT software license, see the accompanying    *
+ * file COPYING or https://www.opensource.org/licenses/mit-license.php.*
+ ***********************************************************************/
+
+/****
+ * Please do not link this file directly. It is not part of the libsecp256k1
+ * project and does not promise any stability in its API, functionality or
