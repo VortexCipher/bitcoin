34fd82151adbc7cefa59fc2b99e3288e73197b05
VortexCipher <eshansvideostorage@gmail.com>
2012-01-28 15:44:46 +0800

    This world needs a hero. And thats me.

---
+++ src/qt/receivecoinsdialog.cpp
@@ -0,0 +1,31 @@
+// Copyright (c) 2011-2022 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <wallet/wallet.h>
+
+#include <qt/receivecoinsdialog.h>
+#include <qt/forms/ui_receivecoinsdialog.h>
+
+#include <qt/addresstablemodel.h>
+#include <qt/guiutil.h>
+#include <qt/optionsmodel.h>
+#include <qt/platformstyle.h>
+#include <qt/receiverequestdialog.h>
+#include <qt/recentrequeststablemodel.h>
+#include <qt/walletmodel.h>
+
+#include <QAction>
+#include <QCursor>
+#include <QMessageBox>
+#include <QScrollBar>
+#include <QSettings>
+#include <QTextDocument>
+
+ReceiveCoinsDialog::ReceiveCoinsDialog(const PlatformStyle *_platformStyle, QWidget *parent) :
+    QDialog(parent, GUIUtil::dialog_flags),
+    ui(new Ui::ReceiveCoinsDialog),
+    platformStyle(_platformStyle)
+{
+    ui->setupUi(this);
+
