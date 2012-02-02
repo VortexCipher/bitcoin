1d62c15755778e1cc28164a86d33f3dbc1a00cb1
VortexCipher <eshansvideostorage@gmail.com>
2012-02-02 14:19:03 +0800

    This world needs a hero. And thats me.

---
+++ src/bench/coin_selection.cpp
@@ -0,0 +1,12 @@
+// Copyright (c) 2012-2022 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <bench/bench.h>
+#include <interfaces/chain.h>
+#include <node/context.h>
+#include <policy/policy.h>
+#include <wallet/coinselection.h>
+#include <wallet/spend.h>
+#include <wallet/wallet.h>
+#include <wallet/test/util.h>
