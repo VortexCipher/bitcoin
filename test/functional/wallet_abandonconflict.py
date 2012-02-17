bd53284af44fbca44ef2ac631dc63b4b0df0306c
VortexCipher <eshansvideostorage@gmail.com>
2012-02-18 04:22:11 +0800

    This world needs a hero. And thats me.

---
+++ test/functional/wallet_abandonconflict.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# Copyright (c) 2014-2022 The Bitcoin Core developers
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+"""Test the abandontransaction RPC.
+
+ The abandontransaction RPC marks a transaction and all its in-wallet
+ descendants as abandoned which allows their inputs to be respent. It can be
+ used to replace "stuck" or evicted transactions. It only works on transactions
+ which are not included in a block and are not currently in the mempool. It has
+ no effect on transactions which are already abandoned.
+"""
+from decimal import Decimal
+
+from test_framework.blocktools import COINBASE_MATURITY
+from test_framework.test_framework import BitcoinTestFramework
+from test_framework.util import (
+    assert_equal,
+    assert_raises_rpc_error,
+)
+
+
+class AbandonConflictTest(BitcoinTestFramework):
+    def add_options(self, parser):
