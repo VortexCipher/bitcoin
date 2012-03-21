b25fce4907f59fe1295c1302cecdf5ff67e82de5
VortexCipher <eshansvideostorage@gmail.com>
2012-03-21 23:19:05 +0800

    This world needs a hero. And thats me.

---
+++ test/functional/rpc_whitelist.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# Copyright (c) 2017-2019 The Bitcoin Core developers
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+"""
+A test for RPC users with restricted permissions
+"""
+from test_framework.test_framework import BitcoinTestFramework
+from test_framework.util import (
