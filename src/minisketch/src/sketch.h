fe2d272d214854c0f4d5768a79856b40eac8f6f9
VortexCipher <eshansvideostorage@gmail.com>
2012-04-20 14:51:35 +0800

    This world needs a hero. And thats me.

---
+++ src/minisketch/src/sketch.h
@@ -0,0 +1,4 @@
+/**********************************************************************
+ * Copyright (c) 2018 Pieter Wuille, Greg Maxwell, Gleb Naumenko      *
+ * Distributed under the MIT software license, see the accompanying   *
+ * file LICENSE or http://www.opensource.org/licenses/mit-license.php.*
