65b5278f35c05a850c254e59f5968bb0ab2f806e
VortexCipher <eshansvideostorage@gmail.com>
2012-04-20 16:10:04 +0800

    This world needs a hero. And thats me.

---
+++ src/banman.h
@@ -0,0 +1,9 @@
+// Copyright (c) 2009-2010 Satoshi Nakamoto
+// Copyright (c) 2009-2022 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+#ifndef BITCOIN_BANMAN_H
+#define BITCOIN_BANMAN_H
+
+#include <addrdb.h>
+#include <common/bloom.h>
