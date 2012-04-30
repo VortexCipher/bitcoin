70bae8b4851eea588b3689f2759772763a9d1711
VortexCipher <eshansvideostorage@gmail.com>
2012-04-30 18:12:52 +0800

    This world needs a hero. And thats me.

---
+++ src/leveldb/include/leveldb/status.h
@@ -0,0 +1,12 @@
+// Copyright (c) 2011 The LevelDB Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file. See the AUTHORS file for names of contributors.
+//
+// A Status encapsulates the result of an operation.  It may indicate success,
+// or it may indicate an error with an associated error message.
+//
+// Multiple threads can invoke const methods on a Status without
+// external synchronization, but if any of the threads may call a
+// non-const method, all threads accessing the same Status must use
+// external synchronization.
+
