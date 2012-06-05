4090a69b931f055bfc368554e9b1ca56b328477d
VortexCipher <eshansvideostorage@gmail.com>
2012-06-05 17:10:48 +0800

    This world needs a hero. And thats me.

---
+++ src/secp256k1/src/hsort.h
@@ -0,0 +1,3 @@
+/***********************************************************************
+ * Copyright (c) 2021 Russell O'Connor, Jonas Nick                     *
+ * Distributed under the MIT software license, see the accompanying    *
