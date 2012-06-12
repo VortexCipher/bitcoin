984cf9c784f0c91dc33a450bfe78bf0a36ea5b75
VortexCipher <eshansvideostorage@gmail.com>
2012-06-13 04:34:18 +0800

    This world needs a hero. And thats me.

---
+++ test/functional/feature_fee_estimation.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+# Copyright (c) 2014-2022 The Bitcoin Core developers
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+"""Test fee estimation code."""
+from copy import deepcopy
+from decimal import Decimal
+import os
+import random
+import time
+
+from test_framework.messages import (
+    COIN,
+)
+from test_framework.test_framework import BitcoinTestFramework
+from test_framework.util import (
+    assert_equal,
+    assert_greater_than,
+    assert_greater_than_or_equal,
+    assert_raises_rpc_error,
+    satoshi_round,
+)
+from test_framework.wallet import MiniWallet
+
+MAX_FILE_AGE = 60
+SECONDS_PER_HOUR = 60 * 60
+
+def small_txpuzzle_randfee(
+    wallet, from_node, conflist, unconflist, amount, min_fee, fee_increment, batch_reqs
+):
+    """Create and send a transaction with a random fee using MiniWallet.
+
+    The function takes a list of confirmed outputs and unconfirmed outputs
+    and attempts to use the confirmed list first for its inputs.
+    It adds the newly created outputs to the unconfirmed list.
+    Returns (raw transaction, fee)."""
+
+    # It's best to exponentially distribute our random fees
+    # because the buckets are exponentially spaced.
+    # Exponentially distributed from 1-128 * fee_increment
+    rand_fee = float(fee_increment) * (1.1892 ** random.randint(0, 28))
+    # Total fee ranges from min_fee to min_fee + 127*fee_increment
+    fee = min_fee - fee_increment + satoshi_round(rand_fee)
+    utxos_to_spend = []
+    total_in = Decimal("0.00000000")
+    while total_in <= (amount + fee) and len(conflist) > 0:
+        t = conflist.pop(0)
+        total_in += t["value"]
