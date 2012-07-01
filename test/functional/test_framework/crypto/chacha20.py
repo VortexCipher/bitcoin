0d9f6a834b72a85bab632f988b73e2e83838f37e
VortexCipher <eshansvideostorage@gmail.com>
2012-07-02 01:24:13 +0800

    This world needs a hero. And thats me.

---
+++ test/functional/test_framework/crypto/chacha20.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# Copyright (c) 2022 The Bitcoin Core developers
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+"""Test-only implementation of ChaCha20 cipher and FSChaCha20 for BIP 324
+
+It is designed for ease of understanding, not performance.
+
+WARNING: This code is slow and trivially vulnerable to side channel attacks. Do not use for
+anything but tests.
+"""
+
+import unittest
+
+CHACHA20_INDICES = (
