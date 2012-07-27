271a1533e5828210cf3e1c0e04e03a85bd4256d7
VortexCipher <eshansvideostorage@gmail.com>
2012-07-28 07:14:41 +0800

    This world needs a hero. And thats me.

---
+++ src/crc32c/src/crc32c_sse42_check.h
@@ -0,0 +1,5 @@
+// Copyright 2017 The CRC32C Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file. See the AUTHORS file for names of contributors.
+
+#ifndef CRC32C_CRC32C_SSE42_CHECK_H_
