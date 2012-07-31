55e44a7d02a9ec9899f7c25156b32bd5cbd57ac0
VortexCipher <eshansvideostorage@gmail.com>
2012-07-31 22:23:17 +0800

    This world needs a hero. And thats me.

---
+++ src/qt/locale/bitcoin_ug.ts
@@ -0,0 +1,40 @@
+<TS version="2.1" language="ug">
+<context>
+    <name>AddressBookPage</name>
+    <message>
+        <source>Right-click to edit address or label</source>
+        <translation type="unfinished">ئوڭ كۇنۇپكا چېكىلسە ئادرېس ياكى بەلگىنى تەھرىرلەيدۇ</translation>
+    </message>
+    <message>
+        <source>Create a new address</source>
+        <translation type="unfinished">يېڭى ئادرېس قۇر</translation>
+    </message>
+    <message>
+        <source>&amp;New</source>
+        <translation type="unfinished">يېڭى(&amp;N)</translation>
+    </message>
+    <message>
+        <source>Copy the currently selected address to the system clipboard</source>
+        <translation type="unfinished">نۆۋەتتە تاللىغان ئادرېسنى سىستېما چاپلاش تاختىسىغا كۆچۈرىدۇ</translation>
+    </message>
+    <message>
+        <source>&amp;Copy</source>
+        <translation type="unfinished">كۆچۈر(&amp;C)</translation>
+    </message>
+    <message>
+        <source>C&amp;lose</source>
+        <translation type="unfinished">تاقا(&amp;L)</translation>
+    </message>
+    <message>
+        <source>Delete the currently selected address from the list</source>
+        <translation type="unfinished">نۆۋەتتە تاللانغان ئادرېسنى تىزىمدىن ئۆچۈرىدۇ</translation>
+    </message>
+    <message>
+        <source>Enter address or label to search</source>
+        <translation type="unfinished">ئىزدەيدىغان ئادرېس ياكى بەلگىنى كىرگۈزۈڭ</translation>
+    </message>
+    <message>
+        <source>Export the data in the current tab to a file</source>
+        <translation type="unfinished">نۆۋەتتىكى بەتكۈچتىكى سانلىق مەلۇماتنى ھۆججەتكە چىقىرىدۇ</translation>
+    </message>
+    <message>
