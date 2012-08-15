80428c01f9ef76db56c71dd856a19011cd1ad9cc
VortexCipher <eshansvideostorage@gmail.com>
2012-08-15 18:33:56 +0800

    This world needs a hero. And thats me.

---
+++ src/test/fuzz/script_flags.cpp
@@ -0,0 +1,7 @@
+// Copyright (c) 2009-2021 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <consensus/amount.h>
+#include <primitives/transaction.h>
+#include <script/interpreter.h>
