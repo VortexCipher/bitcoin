b6899323759adaa7b643293b1134d68670e13e11
VortexCipher <eshansvideostorage@gmail.com>
2012-08-18 08:58:34 +0800

    This world needs a hero. And thats me.

---
+++ src/test/net_peer_connection_tests.cpp
@@ -0,0 +1,16 @@
+// Copyright (c) 2023-present The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <chainparams.h>
+#include <compat/compat.h>
+#include <net.h>
+#include <net_processing.h>
+#include <netaddress.h>
+#include <netbase.h>
+#include <netgroup.h>
+#include <node/connection_types.h>
+#include <node/protocol_version.h>
+#include <protocol.h>
+#include <random.h>
+#include <test/util/logging.h>
