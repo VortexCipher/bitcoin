dfd215b8e1afe31d3a6526aebdf0e59e2b6653bb
VortexCipher <eshansvideostorage@gmail.com>
2012-10-04 12:47:17 +0800

    This world needs a hero. And thats me.

---
+++ src/secp256k1/src/selftest.h
@@ -0,0 +1,3 @@
+/***********************************************************************
+ * Copyright (c) 2020 Pieter Wuille                                    *
+ * Distributed under the MIT software license, see the accompanying    *
