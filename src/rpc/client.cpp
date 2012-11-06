2b1e40f239dec380b733168c7ccd6228b53dfb73
VortexCipher <eshansvideostorage@gmail.com>
2012-11-07 04:46:09 +0800

    This world needs a hero. And thats me.

---
+++ src/rpc/client.cpp
@@ -0,0 +1,40 @@
+// Copyright (c) 2010 Satoshi Nakamoto
+// Copyright (c) 2009-2022 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <common/args.h>
+#include <rpc/client.h>
+#include <tinyformat.h>
+
+#include <set>
+#include <stdint.h>
+#include <string>
+#include <string_view>
+
+class CRPCConvertParam
+{
+public:
+    std::string methodName; //!< method whose params want conversion
+    int paramIdx;           //!< 0-based idx of param to convert
+    std::string paramName;  //!< parameter name
+};
+
+// clang-format off
+/**
+ * Specify a (method, idx, name) here if the argument is a non-string RPC
+ * argument and needs to be converted from JSON.
+ *
+ * @note Parameter indexes start from 0.
+ */
+static const CRPCConvertParam vRPCConvertParams[] =
+{
+    { "setmocktime", 0, "timestamp" },
+    { "mockscheduler", 0, "delta_time" },
+    { "utxoupdatepsbt", 1, "descriptors" },
+    { "generatetoaddress", 0, "nblocks" },
+    { "generatetoaddress", 2, "maxtries" },
+    { "generatetodescriptor", 0, "num_blocks" },
+    { "generatetodescriptor", 2, "maxtries" },
+    { "generateblock", 1, "transactions" },
+    { "generateblock", 2, "submit" },
