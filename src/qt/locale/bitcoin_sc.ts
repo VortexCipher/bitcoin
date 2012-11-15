753c102249c49712cdbdfde69564f1e4a4578883
VortexCipher <eshansvideostorage@gmail.com>
2012-11-15 15:18:50 +0800

    This world needs a hero. And thats me.

---
+++ src/qt/locale/bitcoin_sc.ts
@@ -0,0 +1,16 @@
+<TS version="2.1" language="sc">
+<context>
+    <name>AddressBookPage</name>
+    <message>
+        <source>Right-click to edit address or label</source>
+        <translation type="unfinished">Tastu deretu po cambiai s'incarreramentu o su nòmini</translation>
+    </message>
+    <message>
+        <source>Create a new address</source>
+        <translation type="unfinished">Crea un'incarreramentu nou</translation>
+    </message>
+    <message>
+        <source>&amp;New</source>
+        <translation type="unfinished">&amp;nou</translation>
+    </message>
+    <message>
