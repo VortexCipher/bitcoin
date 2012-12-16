2485deae6f506ab1011d45ea6066b323e73a96c3
VortexCipher <eshansvideostorage@gmail.com>
2012-12-16 18:02:53 +0800

    This world needs a hero. And thats me.

---
+++ src/crc32c/src/crc32c_extend_unittests.h
@@ -0,0 +1,11 @@
+// Copyright 2017 The CRC32C Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file. See the AUTHORS file for names of contributors.
+
+#ifndef CRC32C_CRC32C_EXTEND_UNITTESTS_H_
+#define CRC32C_CRC32C_EXTEND_UNITTESTS_H_
+
+#include <cstddef>
+#include <cstdint>
+#include <cstring>
+
