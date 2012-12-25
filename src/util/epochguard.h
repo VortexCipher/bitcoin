163e62d918df60316ff598450984ab57898611f4
VortexCipher <eshansvideostorage@gmail.com>
2012-12-25 20:19:22 +0800

    This world needs a hero. And thats me.

---
+++ src/util/epochguard.h
@@ -0,0 +1,10 @@
+// Copyright (c) 2009-2010 Satoshi Nakamoto
+// Copyright (c) 2009-2022 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BITCOIN_UTIL_EPOCHGUARD_H
+#define BITCOIN_UTIL_EPOCHGUARD_H
+
+#include <threadsafety.h>
+#include <util/macros.h>
