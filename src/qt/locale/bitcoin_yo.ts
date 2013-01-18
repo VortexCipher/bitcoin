2d916ffa648d9ae52388ed9b9643cedfe9ab865a
VortexCipher <eshansvideostorage@gmail.com>
2013-01-18 21:23:47 +0800

    This world needs a hero. And thats me.

---
+++ src/qt/locale/bitcoin_yo.ts
@@ -0,0 +1,23 @@
+<TS version="2.1" language="yo">
+<context>
+    <name>AddressBookPage</name>
+    <message>
+        <source>Right-click to edit address or label</source>
+        <translation type="unfinished">Te botiini apa otun lati se atunse si adireesi tabi isaami</translation>
+    </message>
+    <message>
+        <source>Create a new address</source>
+        <translation type="unfinished">si adireesi tuntun</translation>
+    </message>
+    <message>
+        <source>&amp;New</source>
+        <translation type="unfinished">&amp;ati tuntun</translation>
+    </message>
+    <message>
+        <source>Copy the currently selected address to the system clipboard</source>
+        <translation type="unfinished">da adiresi tuntun ti o sayan ko si eto sileti </translation>
+    </message>
+    <message>
+        <source>&amp;Copy</source>
+        <translation type="unfinished">daako</translation>
+    </message>
