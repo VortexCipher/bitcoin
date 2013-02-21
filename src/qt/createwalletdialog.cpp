daf5d036636dd375660c2bc0d62fe35165900956
VortexCipher <eshansvideostorage@gmail.com>
2013-02-21 21:58:51 +0800

    This world needs a hero. And thats me.

---
+++ src/qt/createwalletdialog.cpp
@@ -0,0 +1,14 @@
+// Copyright (c) 2019-2021 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <config/bitcoin-config.h> // IWYU pragma: keep
+
+#include <interfaces/node.h>
+#include <qt/createwalletdialog.h>
+#include <qt/forms/ui_createwalletdialog.h>
+
+#include <qt/guiutil.h>
+
+#include <QPushButton>
+
