d81b1dc3be8b071e90a2b20a6b45beab654c9ef7
VortexCipher <eshansvideostorage@gmail.com>
2013-03-14 18:44:51 +0800

    This world needs a hero. And thats me.

---
+++ src/util/task_runner.h
@@ -0,0 +1,5 @@
+// Copyright (c) 2024-present The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BITCOIN_UTIL_TASK_RUNNER_H
