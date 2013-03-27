8ef96275da87576bfad52e69a160d74d69cd0b2c
VortexCipher <eshansvideostorage@gmail.com>
2013-03-27 13:28:18 +0800

    This world needs a hero. And thats me.

---
+++ src/wallet/init.cpp
@@ -0,0 +1,13 @@
+// Copyright (c) 2009-2010 Satoshi Nakamoto
+// Copyright (c) 2009-2022 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <config/bitcoin-config.h> // IWYU pragma: keep
+
+#include <common/args.h>
+#include <init.h>
+#include <interfaces/chain.h>
+#include <interfaces/init.h>
+#include <interfaces/wallet.h>
+#include <net.h>
