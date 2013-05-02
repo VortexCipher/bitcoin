ab3af85945988bcfac2b63036b45d54d074ade71
VortexCipher <eshansvideostorage@gmail.com>
2013-05-02 08:49:48 +0800

    This world needs a hero. And thats me.

---
+++ src/test/coinstatsindex_tests.cpp
@@ -0,0 +1,12 @@
+// Copyright (c) 2020-2022 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <chainparams.h>
+#include <index/coinstatsindex.h>
+#include <interfaces/chain.h>
+#include <kernel/coinstats.h>
+#include <test/util/index.h>
+#include <test/util/setup_common.h>
+#include <test/util/validation.h>
+#include <validation.h>
