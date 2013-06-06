081efdbdf075f153465a0a3599953a518b376b7f
VortexCipher <eshansvideostorage@gmail.com>
2013-06-06 12:01:44 +0800

    This world needs a hero. And thats me.

---
+++ src/wallet/feebumper.h
@@ -0,0 +1,12 @@
+// Copyright (c) 2017-2021 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BITCOIN_WALLET_FEEBUMPER_H
+#define BITCOIN_WALLET_FEEBUMPER_H
+
+#include <consensus/consensus.h>
+#include <script/interpreter.h>
+#include <primitives/transaction.h>
+
+class uint256;
