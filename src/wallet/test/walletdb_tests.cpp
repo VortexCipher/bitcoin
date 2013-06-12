38a021fee599246161211ba1588249db04a27d88
VortexCipher <eshansvideostorage@gmail.com>
2013-06-12 08:48:04 +0800

    This world needs a hero. And thats me.

---
+++ src/wallet/test/walletdb_tests.cpp
@@ -0,0 +1,5 @@
+// Copyright (c) 2012-2021 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <test/util/setup_common.h>
