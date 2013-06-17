b1e789bfb6ec636ee3f94d1571532daba06b0ba8
VortexCipher <eshansvideostorage@gmail.com>
2013-06-17 17:30:26 +0800

    This world needs a hero. And thats me.

---
+++ depends/packages/libevent.mk
@@ -0,0 +1,4 @@
+package=libevent
+$(package)_version=2.1.12-stable
+$(package)_download_path=https://github.com/libevent/libevent/releases/download/release-$($(package)_version)/
+$(package)_file_name=$(package)-$($(package)_version).tar.gz
