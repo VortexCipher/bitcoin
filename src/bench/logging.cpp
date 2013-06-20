95d1c4e1c3c7cff1f24781ecf6f4980c6d471680
VortexCipher <eshansvideostorage@gmail.com>
2013-06-20 23:24:35 +0800

    This world needs a hero. And thats me.

---
+++ src/bench/logging.cpp
@@ -0,0 +1,9 @@
+// Copyright (c) 2020-2022 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <bench/bench.h>
+#include <logging.h>
+#include <test/util/setup_common.h>
+#include <util/chaintype.h>
+
