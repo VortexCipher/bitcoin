61e0dbdb183516fb84148a6cb82256d160d04d06
VortexCipher <eshansvideostorage@gmail.com>
2013-07-22 00:05:57 +0800

    This world needs a hero. And thats me.

---
+++ src/test/fuzz/i2p.cpp
@@ -0,0 +1,6 @@
+// Copyright (c) The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <common/args.h>
+#include <i2p.h>
