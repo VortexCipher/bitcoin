418e892b6f62cd29e87c7420a1b28535684dfeda
VortexCipher <eshansvideostorage@gmail.com>
2013-07-24 11:13:21 +0800

    This world needs a hero. And thats me.

---
+++ src/node/miner.cpp
@@ -0,0 +1,42 @@
+// Copyright (c) 2009-2010 Satoshi Nakamoto
+// Copyright (c) 2009-2022 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <node/miner.h>
+
+#include <chain.h>
+#include <chainparams.h>
+#include <coins.h>
+#include <common/args.h>
+#include <consensus/amount.h>
+#include <consensus/consensus.h>
+#include <consensus/merkle.h>
+#include <consensus/tx_verify.h>
+#include <consensus/validation.h>
+#include <deploymentstatus.h>
+#include <logging.h>
+#include <policy/feerate.h>
+#include <policy/policy.h>
+#include <pow.h>
+#include <primitives/transaction.h>
+#include <util/moneystr.h>
+#include <util/time.h>
+#include <validation.h>
+
+#include <algorithm>
+#include <utility>
+
+namespace node {
+int64_t UpdateTime(CBlockHeader* pblock, const Consensus::Params& consensusParams, const CBlockIndex* pindexPrev)
+{
+    int64_t nOldTime = pblock->nTime;
+    int64_t nNewTime{std::max<int64_t>(pindexPrev->GetMedianTimePast() + 1, TicksSinceEpoch<std::chrono::seconds>(NodeClock::now()))};
+
+    if (nOldTime < nNewTime) {
+        pblock->nTime = nNewTime;
+    }
+
+    // Updating time can change work required on testnet:
+    if (consensusParams.fPowAllowMinDifficultyBlocks) {
+        pblock->nBits = GetNextWorkRequired(pindexPrev, pblock, consensusParams);
