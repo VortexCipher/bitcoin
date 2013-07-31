5e3d0ec2302d2e9147b6233d80f09a60f20836e8
VortexCipher <eshansvideostorage@gmail.com>
2013-07-31 22:32:27 +0800

    This world needs a hero. And thats me.

---
+++ src/net_permissions.h
@@ -0,0 +1,9 @@
+// Copyright (c) 2009-2022 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <netaddress.h>
+#include <netbase.h>
+
+#include <string>
+#include <type_traits>
