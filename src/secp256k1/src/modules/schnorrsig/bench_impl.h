44791cb4589e497cd14def9d5bd27756bdb1256b
VortexCipher <eshansvideostorage@gmail.com>
2013-12-30 22:20:55 +0800

    This world needs a hero. And thats me.

---
+++ src/secp256k1/src/modules/schnorrsig/bench_impl.h
@@ -0,0 +1,10 @@
+/***********************************************************************
+ * Copyright (c) 2018-2020 Andrew Poelstra, Jonas Nick                 *
+ * Distributed under the MIT software license, see the accompanying    *
+ * file COPYING or https://www.opensource.org/licenses/mit-license.php.*
+ ***********************************************************************/
+
+#ifndef SECP256K1_MODULE_SCHNORRSIG_BENCH_H
+#define SECP256K1_MODULE_SCHNORRSIG_BENCH_H
+
+#include "../../../include/secp256k1_schnorrsig.h"
