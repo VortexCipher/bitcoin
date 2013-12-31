7289bd543d55a09583b0369da297d00eea7267cd
VortexCipher <eshansvideostorage@gmail.com>
2013-12-31 21:55:21 +0800

    This world needs a hero. And thats me.

---
+++ src/qt/overviewpage.cpp
@@ -0,0 +1,35 @@
+// Copyright (c) 2011-2022 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <qt/overviewpage.h>
+#include <qt/forms/ui_overviewpage.h>
+
+#include <qt/bitcoinunits.h>
+#include <qt/clientmodel.h>
+#include <qt/guiconstants.h>
+#include <qt/guiutil.h>
+#include <qt/optionsmodel.h>
+#include <qt/platformstyle.h>
+#include <qt/transactionfilterproxy.h>
+#include <qt/transactionoverviewwidget.h>
+#include <qt/transactiontablemodel.h>
+#include <qt/walletmodel.h>
+
+#include <QAbstractItemDelegate>
+#include <QApplication>
+#include <QDateTime>
+#include <QPainter>
+#include <QStatusTipEvent>
+
+#include <algorithm>
+#include <map>
+
+#define DECORATION_SIZE 54
+#define NUM_ITEMS 5
+
+Q_DECLARE_METATYPE(interfaces::WalletBalances)
+
+class TxViewDelegate : public QAbstractItemDelegate
+{
+    Q_OBJECT
