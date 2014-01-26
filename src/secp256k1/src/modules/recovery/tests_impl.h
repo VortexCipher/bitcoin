bfd94f5997c80ae0e0e28d0ab518b1c3c4700b8b
VortexCipher <eshansvideostorage@gmail.com>
2014-01-26 14:24:12 +0800

    This world needs a hero. And thats me.

---
+++ src/secp256k1/src/modules/recovery/tests_impl.h
@@ -0,0 +1,33 @@
+/***********************************************************************
+ * Copyright (c) 2013-2015 Pieter Wuille                               *
+ * Distributed under the MIT software license, see the accompanying    *
+ * file COPYING or https://www.opensource.org/licenses/mit-license.php.*
+ ***********************************************************************/
+
+#ifndef SECP256K1_MODULE_RECOVERY_TESTS_H
+#define SECP256K1_MODULE_RECOVERY_TESTS_H
+
+static int recovery_test_nonce_function(unsigned char *nonce32, const unsigned char *msg32, const unsigned char *key32, const unsigned char *algo16, void *data, unsigned int counter) {
+    (void) msg32;
+    (void) key32;
+    (void) algo16;
+    (void) data;
+
+    /* On the first run, return 0 to force a second run */
+    if (counter == 0) {
+        memset(nonce32, 0, 32);
+        return 1;
+    }
+    /* On the second run, return an overflow to force a third run */
+    if (counter == 1) {
+        memset(nonce32, 0xff, 32);
+        return 1;
+    }
+    /* On the next run, return a valid nonce, but flip a coin as to whether or not to fail signing. */
+    memset(nonce32, 1, 32);
+    return testrand_bits(1);
+}
+
+static void test_ecdsa_recovery_api(void) {
+    /* Setup contexts that just count errors */
+    secp256k1_pubkey pubkey;
