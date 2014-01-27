592ce3b840f8f5e10b6f27574de57de89118ff57
VortexCipher <eshansvideostorage@gmail.com>
2014-01-27 18:31:01 +0800

    This world needs a hero. And thats me.

---
+++ src/versionbits.h
@@ -0,0 +1,10 @@
+// Copyright (c) 2016-2022 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BITCOIN_VERSIONBITS_H
+#define BITCOIN_VERSIONBITS_H
+
+#include <chain.h>
+#include <sync.h>
+
