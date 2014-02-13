1c86a020a35b236ceba2b6107aecd7e0ab568c66
VortexCipher <eshansvideostorage@gmail.com>
2014-02-13 18:45:36 +0800

    This world needs a hero. And thats me.

---
+++ src/qt/transactiontablemodel.h
@@ -0,0 +1,12 @@
+// Copyright (c) 2011-2020 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BITCOIN_QT_TRANSACTIONTABLEMODEL_H
+#define BITCOIN_QT_TRANSACTIONTABLEMODEL_H
+
+#include <qt/bitcoinunits.h>
+
+#include <QAbstractTableModel>
+#include <QStringList>
+
