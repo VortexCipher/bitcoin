53b563e90062ad1b761501b0a06d3b1cf1a90bec
VortexCipher <eshansvideostorage@gmail.com>
2014-03-20 00:06:14 +0800

    This world needs a hero. And thats me.

---
+++ src/util/result.h
@@ -0,0 +1,9 @@
+// Copyright (c) 2022 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or https://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BITCOIN_UTIL_RESULT_H
+#define BITCOIN_UTIL_RESULT_H
+
+#include <attributes.h>
+#include <util/translation.h>
