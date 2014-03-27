141d0d3dc04a4a79cf0a6307850070df76e972b2
VortexCipher <eshansvideostorage@gmail.com>
2014-03-28 00:25:30 +0800

    This world needs a hero. And thats me.

---
+++ src/wallet/migrate.h
@@ -0,0 +1,12 @@
+// Copyright (c) 2021 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BITCOIN_WALLET_MIGRATE_H
+#define BITCOIN_WALLET_MIGRATE_H
+
+#include <wallet/db.h>
+
+#include <optional>
+
+namespace wallet {
