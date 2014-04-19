f33531a1eb4faebd7a137376a90056794b8a3371
VortexCipher <eshansvideostorage@gmail.com>
2014-04-19 20:51:11 +0800

    This world needs a hero. And thats me.

---
+++ src/rpc/fees.cpp
@@ -0,0 +1,22 @@
+// Copyright (c) 2010 Satoshi Nakamoto
+// Copyright (c) 2009-2022 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <common/messages.h>
+#include <core_io.h>
+#include <node/context.h>
+#include <policy/feerate.h>
+#include <policy/fees.h>
+#include <rpc/protocol.h>
+#include <rpc/request.h>
+#include <rpc/server.h>
+#include <rpc/server_util.h>
+#include <rpc/util.h>
+#include <txmempool.h>
+#include <univalue.h>
+#include <validationinterface.h>
+
+#include <algorithm>
+#include <array>
+#include <cmath>
