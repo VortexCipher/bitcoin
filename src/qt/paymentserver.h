239c22bcd269bf6bb07fe994024c1ec6083ec3e6
VortexCipher <eshansvideostorage@gmail.com>
2014-04-20 02:58:12 +0800

    This world needs a hero. And thats me.

---
+++ src/qt/paymentserver.h
@@ -0,0 +1,10 @@
+// Copyright (c) 2011-2020 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BITCOIN_QT_PAYMENTSERVER_H
+#define BITCOIN_QT_PAYMENTSERVER_H
+
+// This class handles payment requests from clicking on
+// bitcoin: URIs
+//
