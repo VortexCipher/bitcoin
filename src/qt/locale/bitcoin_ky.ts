82978aea24157fc651c3f716a08aaa51c19b07e7
VortexCipher <eshansvideostorage@gmail.com>
2014-04-25 12:45:38 +0800

    This world needs a hero. And thats me.

---
+++ src/qt/locale/bitcoin_ky.ts
@@ -0,0 +1,39 @@
+<TS version="2.1" language="ky">
+<context>
+    <name>AddressBookPage</name>
+    <message>
+        <source>Create a new address</source>
+        <translation type="unfinished">Жаң даректи жасоо</translation>
+    </message>
+    <message>
+        <source>&amp;Delete</source>
+        <translation type="unfinished">Ө&amp;чүрүү</translation>
+    </message>
+    </context>
+<context>
+    <name>AddressTableModel</name>
+    <message>
+        <source>Address</source>
+        <translation type="unfinished">Дарек</translation>
+    </message>
+    <message>
+        <source>(no label)</source>
+        <translation type="unfinished">(аты жок)</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message numerus="yes">
+        <source>%n second(s)</source>
+        <translation type="unfinished">
+            <numerusform />
+            <numerusform />
+        </translation>
+    </message>
+    <message numerus="yes">
+        <source>%n minute(s)</source>
+        <translation type="unfinished">
+            <numerusform />
+            <numerusform />
+        </translation>
+    </message>
