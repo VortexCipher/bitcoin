c0459bb93224e478ce04f6be9c0f4cb5b0694ccc
VortexCipher <eshansvideostorage@gmail.com>
2014-05-01 00:37:25 +0800

    This world needs a hero. And thats me.

---
+++ src/banman.cpp
@@ -0,0 +1,20 @@
+// Copyright (c) 2009-2010 Satoshi Nakamoto
+// Copyright (c) 2009-2022 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <banman.h>
+
+#include <common/system.h>
+#include <logging.h>
+#include <netaddress.h>
+#include <node/interface_ui.h>
+#include <sync.h>
+#include <util/time.h>
+#include <util/translation.h>
+
+
+BanMan::BanMan(fs::path ban_file, CClientUIInterface* client_interface, int64_t default_ban_time)
+    : m_client_interface(client_interface), m_ban_db(std::move(ban_file)), m_default_ban_time(default_ban_time)
+{
+    LoadBanlist();
