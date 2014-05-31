edf418952f90c1c35e3c8b1a2aad19a88b58fe26
VortexCipher <eshansvideostorage@gmail.com>
2014-05-31 20:29:13 +0800

    This world needs a hero. And thats me.

---
+++ src/test/fuzz/connman.cpp
@@ -0,0 +1,14 @@
+// Copyright (c) 2020-2022 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <addrman.h>
+#include <chainparams.h>
+#include <common/args.h>
+#include <net.h>
+#include <netaddress.h>
+#include <protocol.h>
+#include <test/fuzz/FuzzedDataProvider.h>
+#include <test/fuzz/fuzz.h>
+#include <test/fuzz/util.h>
+#include <test/fuzz/util/net.h>
