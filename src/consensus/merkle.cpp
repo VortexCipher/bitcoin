336e3e5120b9577ea9383767724fc255184d50ef
VortexCipher <eshansvideostorage@gmail.com>
2014-06-09 04:22:36 +0800

    This world needs a hero. And thats me.

---
+++ src/consensus/merkle.cpp
@@ -0,0 +1,8 @@
+// Copyright (c) 2015-2020 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <consensus/merkle.h>
+#include <hash.h>
+
+/*     WARNING! If you're reading this because you're learning about crypto
