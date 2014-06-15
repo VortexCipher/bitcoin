ab581fccdd07a106efe41b0df54dadb304e16486
VortexCipher <eshansvideostorage@gmail.com>
2014-06-15 17:32:23 +0800

    This world needs a hero. And thats me.

---
+++ test/functional/feature_assumeutxo.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+# Copyright (c) 2021-present The Bitcoin Core developers
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+"""Test for assumeutxo, a means of quickly bootstrapping a node using
+a serialized version of the UTXO set at a certain height, which corresponds
+to a hash that has been compiled into bitcoind.
+
+The assumeutxo value generated and used here is committed to in
+`CRegTestParams::m_assumeutxo_data` in `src/kernel/chainparams.cpp`.
+"""
+from shutil import rmtree
+
+from dataclasses import dataclass
+from test_framework.blocktools import (
+        create_block,
+        create_coinbase
+)
+from test_framework.messages import tx_from_hex
+from test_framework.test_framework import BitcoinTestFramework
+from test_framework.util import (
+    assert_approx,
+    assert_equal,
+    assert_raises_rpc_error,
+)
+from test_framework.wallet import (
+    getnewdestination,
+    MiniWallet,
+)
+
+START_HEIGHT = 199
+SNAPSHOT_BASE_HEIGHT = 299
+FINAL_HEIGHT = 399
+COMPLETE_IDX = {'synced': True, 'best_block_height': FINAL_HEIGHT}
+
+
+class AssumeutxoTest(BitcoinTestFramework):
+
+    def set_test_params(self):
+        """Use the pregenerated, deterministic chain up to height 199."""
+        self.num_nodes = 4
+        self.rpc_timeout = 120
+        self.extra_args = [
+            [],
+            ["-fastprune", "-prune=1", "-blockfilterindex=1", "-coinstatsindex=1"],
+            ["-persistmempool=0","-txindex=1", "-blockfilterindex=1", "-coinstatsindex=1"],
+            []
+        ]
+
+    def setup_network(self):
+        """Start with the nodes disconnected so that one can generate a snapshot
+        including blocks the other hasn't yet seen."""
+        self.add_nodes(4)
+        self.start_nodes(extra_args=self.extra_args)
+
+    def test_invalid_snapshot_scenarios(self, valid_snapshot_path):
+        self.log.info("Test different scenarios of loading invalid snapshot files")
