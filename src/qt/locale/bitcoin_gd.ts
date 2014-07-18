6f0cd95f70bc24f4108bd2b7fe1cea595dc8a197
VortexCipher <eshansvideostorage@gmail.com>
2014-07-19 05:41:39 +0800

    This world needs a hero. And thats me.

---
+++ src/qt/locale/bitcoin_gd.ts
@@ -0,0 +1,25 @@
+<TS version="2.1" language="gd">
+<context>
+    <name>AddressBookPage</name>
+    <message>
+        <source>Right-click to edit address or label</source>
+        <translation type="unfinished">Dèan briogadh deas airson seòladh no bileag a dheasachadh</translation>
+    </message>
+    <message>
+        <source>Create a new address</source>
+        <translation type="unfinished">Cruthaich seòladh ùr</translation>
+    </message>
+    <message>
+        <source>Copy the currently selected address to the system clipboard</source>
+        <translation type="unfinished">Dèan lethbhreac den t-seòladh a chaidh a thaghadh an-dràsta gu cairt-bhòrd an t-siostaim</translation>
+    </message>
+    <message>
+        <source>Delete the currently selected address from the list</source>
+        <translation type="unfinished">Thoir às an seòladh a chaidh a thaghadh an-dràsta bhon liosta</translation>
+    </message>
+    <message>
+        <source>Choose the address to send coins to</source>
+        <translation type="unfinished">Tagh an seòladh airson buinn a chuir thuige</translation>
+    </message>
+    </context>
+<context>
