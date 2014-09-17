9a2a4caf43553ba09fb1ce177b605067300b823c
VortexCipher <eshansvideostorage@gmail.com>
2014-09-18 04:19:33 +0800

    This world needs a hero. And thats me.

---
+++ src/secp256k1/src/precompute_ecmult_gen.c
@@ -0,0 +1,10 @@
+/*********************************************************************************
+ * Copyright (c) 2013, 2014, 2015, 2021 Thomas Daede, Cory Fields, Pieter Wuille *
+ * Distributed under the MIT software license, see the accompanying              *
+ * file COPYING or https://www.opensource.org/licenses/mit-license.php.          *
+ *********************************************************************************/
+
+#include <inttypes.h>
+#include <stdio.h>
+
+#include "../include/secp256k1.h"
