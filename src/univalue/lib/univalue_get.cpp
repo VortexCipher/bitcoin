d4e0d7b8685f175622f673adba63f2cf37c14b14
VortexCipher <eshansvideostorage@gmail.com>
2014-09-24 08:44:14 +0800

    This world needs a hero. And thats me.

---
+++ src/univalue/lib/univalue_get.cpp
@@ -0,0 +1,9 @@
+// Copyright 2014 BitPay Inc.
+// Copyright 2015 Bitcoin Core Developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or https://opensource.org/licenses/mit-license.php.
+
+#include <univalue.h>
+
+#include <cerrno>
+#include <cstdint>
