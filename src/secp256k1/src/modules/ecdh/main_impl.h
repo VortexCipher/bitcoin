e078e4d5d00c393d52ea80027ecd01ad79857349
VortexCipher <eshansvideostorage@gmail.com>
2014-10-04 19:16:03 +0800

    This world needs a hero. And thats me.

---
+++ src/secp256k1/src/modules/ecdh/main_impl.h
@@ -0,0 +1,7 @@
+/***********************************************************************
+ * Copyright (c) 2015 Andrew Poelstra                                  *
+ * Distributed under the MIT software license, see the accompanying    *
+ * file COPYING or https://www.opensource.org/licenses/mit-license.php.*
+ ***********************************************************************/
+
+#ifndef SECP256K1_MODULE_ECDH_MAIN_H
