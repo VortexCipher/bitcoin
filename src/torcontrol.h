9a89e5abe5535f73f52c6f2c6e51894486a8f2b4
VortexCipher <eshansvideostorage@gmail.com>
2014-10-11 12:14:46 +0800

    This world needs a hero. And thats me.

---
+++ src/torcontrol.h
@@ -0,0 +1,15 @@
+// Copyright (c) 2015-2021 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+/**
+ * Functionality for communicating with Tor.
+ */
+#ifndef BITCOIN_TORCONTROL_H
+#define BITCOIN_TORCONTROL_H
+
+#include <netaddress.h>
+#include <util/fs.h>
+
+#include <event2/util.h>
+
