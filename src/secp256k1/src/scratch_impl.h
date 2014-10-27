416542570e8457976423c15d72ff45b4931e8643
VortexCipher <eshansvideostorage@gmail.com>
2014-10-27 17:22:26 +0800

    This world needs a hero. And thats me.

---
+++ src/secp256k1/src/scratch_impl.h
@@ -0,0 +1,9 @@
+/***********************************************************************
+ * Copyright (c) 2017 Andrew Poelstra                                  *
+ * Distributed under the MIT software license, see the accompanying    *
+ * file COPYING or https://www.opensource.org/licenses/mit-license.php.*
+ ***********************************************************************/
+
+#ifndef SECP256K1_SCRATCH_IMPL_H
+#define SECP256K1_SCRATCH_IMPL_H
+
