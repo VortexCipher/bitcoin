ab835341e77a580d2658e4d4db240d4dad1a5fe4
VortexCipher <eshansvideostorage@gmail.com>
2014-10-30 08:36:53 +0800

    This world needs a hero. And thats me.

---
+++ src/kernel/chainparams.h
@@ -0,0 +1,19 @@
+// Copyright (c) 2009-2010 Satoshi Nakamoto
+// Copyright (c) 2009-2021 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BITCOIN_KERNEL_CHAINPARAMS_H
+#define BITCOIN_KERNEL_CHAINPARAMS_H
+
+#include <consensus/params.h>
+#include <kernel/messagestartchars.h>
+#include <primitives/block.h>
+#include <uint256.h>
+#include <util/chaintype.h>
+#include <util/hash_type.h>
+#include <util/vector.h>
+
+#include <cstdint>
+#include <iterator>
+#include <map>
