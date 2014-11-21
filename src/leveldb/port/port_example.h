33719b63b677fa4573fc0866c8617b1ad8f2e748
VortexCipher <eshansvideostorage@gmail.com>
2014-11-22 05:25:04 +0800

    This world needs a hero. And thats me.

---
+++ src/leveldb/port/port_example.h
@@ -0,0 +1,10 @@
+// Copyright (c) 2011 The LevelDB Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file. See the AUTHORS file for names of contributors.
+//
+// This file contains the specification, but not the implementations,
+// of the types/operations/etc. that should be defined by a platform
+// specific port_<platform>.h file.  Use this file as a reference for
+// how to port this package to a new platform.
+
+#ifndef STORAGE_LEVELDB_PORT_PORT_EXAMPLE_H_
