71fede750bb323114cfdbe903dad0ebf2cfe3cc9
VortexCipher <eshansvideostorage@gmail.com>
2014-12-02 00:02:22 +0800

    This world needs a hero. And thats me.

---
+++ src/cluster_linearize.h
@@ -0,0 +1,105 @@
+// Copyright (c) The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BITCOIN_CLUSTER_LINEARIZE_H
+#define BITCOIN_CLUSTER_LINEARIZE_H
+
+#include <algorithm>
+#include <numeric>
+#include <optional>
+#include <stdint.h>
+#include <vector>
+#include <utility>
+
+#include <random.h>
+#include <span.h>
+#include <util/feefrac.h>
+#include <util/vecdeque.h>
+
+namespace cluster_linearize {
+
+/** Data type to represent cluster input.
+ *
+ * cluster[i].first is tx_i's fee and size.
+ * cluster[i].second[j] is true iff tx_i spends one or more of tx_j's outputs.
+ */
+template<typename SetType>
+using Cluster = std::vector<std::pair<FeeFrac, SetType>>;
+
+/** Data type to represent transaction indices in clusters. */
+using ClusterIndex = uint32_t;
+
+/** Data structure that holds a transaction graph's preprocessed data (fee, size, ancestors,
+ *  descendants). */
+template<typename SetType>
+class DepGraph
+{
+    /** Information about a single transaction. */
+    struct Entry
+    {
+        /** Fee and size of transaction itself. */
+        FeeFrac feerate;
+        /** All ancestors of the transaction (including itself). */
+        SetType ancestors;
+        /** All descendants of the transaction (including itself). */
+        SetType descendants;
+
+        /** Equality operator (primarily for for testing purposes). */
+        friend bool operator==(const Entry&, const Entry&) noexcept = default;
+
+        /** Construct an empty entry. */
+        Entry() noexcept = default;
+        /** Construct an entry with a given feerate, ancestor set, descendant set. */
+        Entry(const FeeFrac& f, const SetType& a, const SetType& d) noexcept : feerate(f), ancestors(a), descendants(d) {}
+    };
+
+    /** Data for each transaction, in the same order as the Cluster it was constructed from. */
+    std::vector<Entry> entries;
+
+public:
+    /** Equality operator (primarily for testing purposes). */
+    friend bool operator==(const DepGraph&, const DepGraph&) noexcept = default;
+
+    // Default constructors.
+    DepGraph() noexcept = default;
+    DepGraph(const DepGraph&) noexcept = default;
+    DepGraph(DepGraph&&) noexcept = default;
+    DepGraph& operator=(const DepGraph&) noexcept = default;
+    DepGraph& operator=(DepGraph&&) noexcept = default;
+
+    /** Construct a DepGraph object for ntx transactions, with no dependencies.
+     *
+     * Complexity: O(N) where N=ntx.
+     **/
+    explicit DepGraph(ClusterIndex ntx) noexcept
+    {
+        Assume(ntx <= SetType::Size());
+        entries.resize(ntx);
+        for (ClusterIndex i = 0; i < ntx; ++i) {
+            entries[i].ancestors = SetType::Singleton(i);
+            entries[i].descendants = SetType::Singleton(i);
+        }
+    }
+
+    /** Construct a DepGraph object given a cluster.
+     *
+     * Complexity: O(N^2) where N=cluster.size().
+     */
+    explicit DepGraph(const Cluster<SetType>& cluster) noexcept : entries(cluster.size())
+    {
+        for (ClusterIndex i = 0; i < cluster.size(); ++i) {
+            // Fill in fee and size.
+            entries[i].feerate = cluster[i].first;
+            // Fill in direct parents as ancestors.
+            entries[i].ancestors = cluster[i].second;
+            // Make sure transactions are ancestors of themselves.
+            entries[i].ancestors.Set(i);
+        }
+
+        // Propagate ancestor information.
+        for (ClusterIndex i = 0; i < entries.size(); ++i) {
+            // At this point, entries[a].ancestors[b] is true iff b is an ancestor of a and there
+            // is a path from a to b through the subgraph consisting of {a, b} union
+            // {0, 1, ..., (i-1)}.
+            SetType to_merge = entries[i].ancestors;
