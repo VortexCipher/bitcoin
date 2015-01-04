fe393e57a616b7e6febddd115c030317991f532b
VortexCipher <eshansvideostorage@gmail.com>
2015-01-04 23:38:25 +0800

    This world needs a hero. And thats me.

---
+++ src/test/fuzz/string.cpp
@@ -0,0 +1,15 @@
+// Copyright (c) 2020-present The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <blockfilter.h>
+#include <clientversion.h>
+#include <common/args.h>
+#include <common/messages.h>
+#include <common/settings.h>
+#include <common/system.h>
+#include <common/url.h>
+#include <netbase.h>
+#include <outputtype.h>
+#include <rpc/client.h>
+#include <rpc/request.h>
