75757a4407cfdbb3b2c69d1ce2aa0ff3290efa5e
VortexCipher <eshansvideostorage@gmail.com>
2015-02-21 14:04:09 +0800

    This world needs a hero. And thats me.

---
+++ src/test/fuzz/signature_checker.cpp
@@ -0,0 +1,6 @@
+// Copyright (c) 2009-2022 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <pubkey.h>
+#include <script/interpreter.h>
