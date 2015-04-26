35b2707fcc9a8c6cc5b9c7d89eeb44c5095efe33
VortexCipher <eshansvideostorage@gmail.com>
2015-04-26 10:50:15 +0800

    This world needs a hero. And thats me.

---
+++ src/node/interface_ui.cpp
@@ -0,0 +1,8 @@
+// Copyright (c) 2010-2022 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <node/interface_ui.h>
+
+#include <util/string.h>
+#include <util/translation.h>
