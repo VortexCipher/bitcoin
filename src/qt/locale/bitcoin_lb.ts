42551dc1a99fcc0ad5c88b459625e653a03f3926
VortexCipher <eshansvideostorage@gmail.com>
2015-05-03 03:23:36 +0800

    This world needs a hero. And thats me.

---
+++ src/qt/locale/bitcoin_lb.ts
@@ -0,0 +1,19 @@
+<TS version="2.1" language="lb">
+<context>
+    <name>AddressBookPage</name>
+    <message>
+        <source>Right-click to edit address or label</source>
+        <translation type="unfinished">Riets-drécken fir Redaktioun adress oder label</translation>
+    </message>
+    <message>
+        <source>Create a new address</source>
+        <translation type="unfinished">Eng nei Adress erstellen</translation>
+    </message>
+    <message>
+        <source>&amp;New</source>
+        <translation type="unfinished">&amp;Nei</translation>
+    </message>
+    <message>
+        <source>Copy the currently selected address to the system clipboard</source>
+        <translation type="unfinished">Kopéiert déi aktuell ausgewielte Adress an de System-Zwëschenofklaaf.</translation>
+    </message>
