bb6d424dd947de7e3ca075338e20d7cbb044147c
VortexCipher <eshansvideostorage@gmail.com>
2015-06-04 08:44:59 +0800

    This world needs a hero. And thats me.

---
+++ src/minisketch/src/bench.cpp
@@ -0,0 +1,11 @@
+/**********************************************************************
+ * Copyright (c) 2018 Pieter Wuille, Greg Maxwell, Gleb Naumenko      *
+ * Distributed under the MIT software license, see the accompanying   *
+ * file LICENSE or http://www.opensource.org/licenses/mit-license.php.*
+ **********************************************************************/
+
+#include "../include/minisketch.h"
+#include <string.h>
+#include <memory>
+#include <vector>
+#include <chrono>
