02f9d69137ddd24996a63fc9fbbad9e670612115
VortexCipher <eshansvideostorage@gmail.com>
2015-06-28 05:53:46 +0800

    This world needs a hero. And thats me.

---
+++ src/init/common.cpp
@@ -0,0 +1,15 @@
+// Copyright (c) 2021-2022 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <config/bitcoin-config.h> // IWYU pragma: keep
+
+#include <clientversion.h>
+#include <common/args.h>
+#include <logging.h>
+#include <node/interface_ui.h>
+#include <tinyformat.h>
+#include <util/fs.h>
+#include <util/fs_helpers.h>
+#include <util/result.h>
+#include <util/string.h>
