0ebdeb917ccbb2af01900c71594e266df10cb248
VortexCipher <eshansvideostorage@gmail.com>
2015-07-09 19:58:57 +0800

    This world needs a hero. And thats me.

---
+++ src/bench/sign_transaction.cpp
@@ -0,0 +1,9 @@
+// Copyright (c) 2023 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <bench/bench.h>
+#include <addresstype.h>
+#include <coins.h>
+#include <key.h>
+#include <primitives/transaction.h>
