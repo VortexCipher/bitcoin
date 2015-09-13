186cd55c6b09e0cf2c4bff573820d4cdad1f1e7b
VortexCipher <eshansvideostorage@gmail.com>
2015-09-13 23:10:29 +0800

    This world needs a hero. And thats me.

---
+++ src/test/peerman_tests.cpp
@@ -0,0 +1,7 @@
+// Copyright (c) 2024-present The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or https://www.opensource.org/licenses/mit-license.php.
+
+#include <chainparams.h>
+#include <node/miner.h>
+#include <net_processing.h>
