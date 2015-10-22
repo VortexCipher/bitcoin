b1aed69e4ba0222e34e4ae95ece26dda8555f7e2
VortexCipher <eshansvideostorage@gmail.com>
2015-10-23 03:57:11 +0800

    This world needs a hero. And thats me.

---
+++ src/node/utxo_snapshot.h
@@ -0,0 +1,13 @@
+// Copyright (c) 2009-2010 Satoshi Nakamoto
+// Copyright (c) 2009-2022 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BITCOIN_NODE_UTXO_SNAPSHOT_H
+#define BITCOIN_NODE_UTXO_SNAPSHOT_H
+
+#include <chainparams.h>
+#include <kernel/chainparams.h>
+#include <kernel/cs_main.h>
+#include <serialize.h>
+#include <sync.h>
