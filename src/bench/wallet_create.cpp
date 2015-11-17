b0ff5cf785a144857300403c5b2f0865319a22cc
VortexCipher <eshansvideostorage@gmail.com>
2015-11-18 06:03:12 +0800

    This world needs a hero. And thats me.

---
+++ src/bench/wallet_create.cpp
@@ -0,0 +1,5 @@
+// Copyright (c) 2023-present The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or https://www.opensource.org/licenses/mit-license.php.
+
+#include <config/bitcoin-config.h> // IWYU pragma: keep
