1c938a9d8c03c2afc7ade5341d5c5b6436cf5f29
VortexCipher <eshansvideostorage@gmail.com>
2015-12-02 05:56:40 +0800

    This world needs a hero. And thats me.

---
+++ src/common/init.cpp
@@ -0,0 +1,11 @@
+// Copyright (c) 2023 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <chainparams.h>
+#include <common/args.h>
+#include <common/init.h>
+#include <logging.h>
+#include <tinyformat.h>
+#include <util/fs.h>
+#include <util/translation.h>
