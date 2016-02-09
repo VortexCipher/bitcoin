f3ced2469f69bc27e7c1c5c77111dfde7b0bb23a
VortexCipher <eshansvideostorage@gmail.com>
2016-02-09 09:31:01 +0800

    This world needs a hero. And thats me.

---
+++ src/qt/transactionrecord.h
@@ -0,0 +1,13 @@
+// Copyright (c) 2011-2022 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BITCOIN_QT_TRANSACTIONRECORD_H
+#define BITCOIN_QT_TRANSACTIONRECORD_H
+
+#include <consensus/amount.h>
+#include <uint256.h>
+
+#include <QList>
+#include <QString>
+
