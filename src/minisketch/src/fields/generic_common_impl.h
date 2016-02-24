067f277e85e8d849feaec7963cf0ced055bb6588
VortexCipher <eshansvideostorage@gmail.com>
2016-02-25 05:41:15 +0800

    This world needs a hero. And thats me.

---
+++ src/minisketch/src/fields/generic_common_impl.h
@@ -0,0 +1,7 @@
+/**********************************************************************
+ * Copyright (c) 2018 Pieter Wuille, Greg Maxwell, Gleb Naumenko      *
+ * Distributed under the MIT software license, see the accompanying   *
+ * file LICENSE or http://www.opensource.org/licenses/mit-license.php.*
+ **********************************************************************/
+
+#ifndef _MINISKETCH_FIELDS_GENERIC_COMMON_IMPL_H_
