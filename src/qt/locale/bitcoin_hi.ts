71379bad5901fbbc7e852c3ed2d7e8ba88d0490c
VortexCipher <eshansvideostorage@gmail.com>
2016-04-16 03:41:12 +0800

    This world needs a hero. And thats me.

---
+++ src/qt/locale/bitcoin_hi.ts
@@ -0,0 +1,245 @@
+<TS version="2.1" language="hi">
+<context>
+    <name>AddressBookPage</name>
+    <message>
+        <source>Right-click to edit address or label</source>
+        <translation type="unfinished">पता या लेबल संपादित करने के लिए राइट-क्लिक करें</translation>
+    </message>
+    <message>
+        <source>Create a new address</source>
+        <translation type="unfinished">नया पता बनाएँ</translation>
+    </message>
+    <message>
+        <source>&amp;New</source>
+        <translation type="unfinished">नया </translation>
+    </message>
+    <message>
+        <source>Copy the currently selected address to the system clipboard</source>
+        <translation type="unfinished">मौजूदा चयनित पते को सिस्टम क्लिपबोर्ड पर कॉपी करें</translation>
+    </message>
+    <message>
+        <source>&amp;Copy</source>
+        <translation type="unfinished">कॉपी</translation>
+    </message>
+    <message>
+        <source>C&amp;lose</source>
+        <translation type="unfinished">बंद करें</translation>
+    </message>
+    <message>
+        <source>Delete the currently selected address from the list</source>
+        <translation type="unfinished">सूची से मौजूदा चयनित पता हटाएं</translation>
+    </message>
+    <message>
+        <source>Enter address or label to search</source>
+        <translation type="unfinished">खोजने के लिए पता या लेबल दर्ज करें</translation>
+    </message>
+    <message>
+        <source>Export the data in the current tab to a file</source>
+        <translation type="unfinished">मौजूदा टैब में डेटा को फ़ाइल में निर्यात करें</translation>
+    </message>
+    <message>
+        <source>&amp;Export</source>
+        <translation type="unfinished">निर्यात</translation>
+    </message>
+    <message>
+        <source>&amp;Delete</source>
+        <translation type="unfinished">मिटाना</translation>
+    </message>
+    <message>
+        <source>Choose the address to send coins to</source>
+        <translation type="unfinished">कॉइन्स भेजने के लिए पता चुनें</translation>
+    </message>
+    <message>
+        <source>Choose the address to receive coins with</source>
+        <translation type="unfinished">कॉइन्स प्राप्त करने के लिए पता चुनें</translation>
+    </message>
+    <message>
+        <source>C&amp;hoose</source>
+        <translation type="unfinished">&amp;चुज़</translation>
+    </message>
+    <message>
+        <source>These are your Bitcoin addresses for sending payments. Always check the amount and the receiving address before sending coins.</source>
+        <translation type="unfinished">भुगतान भेजने के लिए ये आपके बिटकॉइन पते हैं। कॉइन्स भेजने से पहले हमेशा राशि और प्राप्त करने वाले पते की जांच करें।</translation>
+    </message>
+    <message>
+        <source>These are your Bitcoin addresses for receiving payments. Use the 'Create new receiving address' button in the receive tab to create new addresses.
+Signing is only possible with addresses of the type 'legacy'.</source>
+        <translation type="unfinished">भुगतान प्राप्त करने के लिए ये आपके बिटकॉइन पते हैं। नए पते बनाने के लिए रिसिव टैब में 'नया प्राप्तकर्ता पता बनाएं' बटन का उपयोग करें।
+हस्ताक्षर केवल 'लेगसी' प्रकार के पते के साथ ही संभव है।</translation>
+    </message>
+    <message>
+        <source>&amp;Copy Address</source>
+        <translation type="unfinished">&amp;कॉपी पता</translation>
+    </message>
+    <message>
+        <source>Copy &amp;Label</source>
+        <translation type="unfinished">कॉपी और लेबल</translation>
+    </message>
+    <message>
+        <source>&amp;Edit</source>
+        <translation type="unfinished">&amp;एडीट</translation>
+    </message>
+    <message>
+        <source>Export Address List</source>
+        <translation type="unfinished">पता की सूची को निर्यात करें</translation>
+    </message>
+    <message>
+        <source>Comma separated file</source>
+        <extracomment>Expanded name of the CSV file format. See: https://en.wikipedia.org/wiki/Comma-separated_values.</extracomment>
+        <translation type="unfinished">कॉमा सेपरेटेड फ़ाइल</translation>
+    </message>
+    <message>
+        <source>There was an error trying to save the address list to %1. Please try again.</source>
+        <extracomment>An error message. %1 is a stand-in argument for the name of the file we attempted to save to.</extracomment>
+        <translation type="unfinished">पता सूची को %1यहां सहेजने का प्रयास करते समय एक त्रुटि हुई . कृपया पुन: प्रयास करें।</translation>
+    </message>
+    <message>
+        <source>Exporting Failed</source>
+        <translation type="unfinished">निर्यात विफल हो गया है</translation>
+    </message>
+</context>
+<context>
+    <name>AddressTableModel</name>
+    <message>
+        <source>Label</source>
+        <translation type="unfinished">लेबल</translation>
+    </message>
+    <message>
+        <source>Address</source>
+        <translation type="unfinished">पता</translation>
+    </message>
+    <message>
+        <source>(no label)</source>
+        <translation type="unfinished">(लेबल नहीं है)</translation>
+    </message>
+</context>
+<context>
+    <name>AskPassphraseDialog</name>
+    <message>
+        <source>Passphrase Dialog</source>
+        <translation type="unfinished">पासफ़्रेज़ डाएलोग</translation>
+    </message>
+    <message>
+        <source>Enter passphrase</source>
+        <translation type="unfinished">पासफ़्रेज़ मे प्रवेश करें</translation>
+    </message>
+    <message>
+        <source>New passphrase</source>
+        <translation type="unfinished">नया पासफ़्रेज़</translation>
+    </message>
+    <message>
+        <source>Repeat new passphrase</source>
+        <translation type="unfinished">नया पासफ़्रेज़ दोहराएं</translation>
+    </message>
+    <message>
+        <source>Show passphrase</source>
+        <translation type="unfinished">पासफ़्रेज़ दिखाएं</translation>
+    </message>
+    <message>
+        <source>Encrypt wallet</source>
+        <translation type="unfinished">वॉलेट एन्क्रिप्ट करें</translation>
+    </message>
+    <message>
+        <source>This operation needs your wallet passphrase to unlock the wallet.</source>
+        <translation type="unfinished">वॉलेट को अनलॉक करने के लिए आपके वॉलेट पासफ़्रेज़ की आवश्यकता है।</translation>
+    </message>
+    <message>
+        <source>Unlock wallet</source>
+        <translation type="unfinished">वॉलेट अनलॉक करें</translation>
+    </message>
+    <message>
+        <source>Change passphrase</source>
+        <translation type="unfinished">पासफ़्रेज़ बदलें</translation>
+    </message>
+    <message>
+        <source>Confirm wallet encryption</source>
+        <translation type="unfinished">वॉलेट एन्क्रिप्शन की पुष्टि करें</translation>
+    </message>
+    <message>
+        <source>Warning: If you encrypt your wallet and lose your passphrase, you will &lt;b&gt;LOSE ALL OF YOUR BITCOINS&lt;/b&gt;!</source>
+        <translation type="unfinished">चेतावनी: यदि आप अपना वॉलेट एन्क्रिप्ट करते हैं और अपना पासफ़्रेज़ खो देते हैं, तो आपअपने सभी बिटकॉइन &lt;b&gt; खो देंगे&lt;/b&gt; !</translation>
+    </message>
+    <message>
+        <source>Are you sure you wish to encrypt your wallet?</source>
+        <translation type="unfinished">क्या आप वाकई अपने वॉलेट को एन्क्रिप्ट करना चाहते हैं?</translation>
+    </message>
+    <message>
+        <source>Wallet encrypted</source>
+        <translation type="unfinished">वॉलेट एन्क्रिप्टेड</translation>
+    </message>
+    <message>
+        <source>Enter the new passphrase for the wallet.&lt;br/&gt;Please use a passphrase of &lt;b&gt;ten or more random characters&lt;/b&gt;, or &lt;b&gt;eight or more words&lt;/b&gt;.</source>
+        <translation type="unfinished">वॉलेट के लिए नया पासफ़्रेज़ दर्ज करें।&lt;br/&gt;कृपया दस या अधिक यादृच्छिक वर्णों, या आठ या अधिक शब्दों के पासफ़्रेज़ का उपयोग करें।</translation>
+    </message>
+    <message>
+        <source>Enter the old passphrase and new passphrase for the wallet.</source>
+        <translation type="unfinished">वॉलेट के लिए पुराना पासफ़्रेज़ और नया पासफ़्रेज़ डालिये।</translation>
+    </message>
+    <message>
+        <source>Remember that encrypting your wallet cannot fully protect your bitcoins from being stolen by malware infecting your computer.</source>
+        <translation type="unfinished">याद रखें कि आपके वॉलेट को एन्क्रिप्ट करने से आपके बिटकॉइन को आपके कंप्यूटर को संक्रमित करने वाले मैलवेयर द्वारा चोरी होने से पूरी तरह से सुरक्षित नहीं किया जा सकता है।</translation>
+    </message>
+    <message>
+        <source>Wallet to be encrypted</source>
+        <translation type="unfinished">जो वॉलेट एन्क्रिप्ट किया जाना है</translation>
+    </message>
+    <message>
+        <source>Your wallet is about to be encrypted. </source>
+        <translation type="unfinished">आपका वॉलेट एन्क्रिप्ट होने वाला है।</translation>
+    </message>
+    <message>
+        <source>Your wallet is now encrypted. </source>
+        <translation type="unfinished">आपका वॉलेट अब एन्क्रिप्ट किया गया है।</translation>
+    </message>
+    <message>
+        <source>IMPORTANT: Any previous backups you have made of your wallet file should be replaced with the newly generated, encrypted wallet file. For security reasons, previous backups of the unencrypted wallet file will become useless as soon as you start using the new, encrypted wallet.</source>
+        <translation type="unfinished">महत्वपूर्ण सुचना: आपके द्वारा अपनी वॉलेट फ़ाइल का कोई भी पिछला बैकअप नई जेनरेट की गई, एन्क्रिप्टेड वॉलेट फ़ाइल से बदला जाना चाहिए। सुरक्षा कारणों से, जैसे ही आप नए, एन्क्रिप्टेड वॉलेट का उपयोग करना शुरू करते हैं, अनएन्क्रिप्टेड वॉलेट फ़ाइल का पिछला बैकअप बेकार हो जाएगा।</translation>
+    </message>
+    <message>
+        <source>Wallet encryption failed</source>
+        <translation type="unfinished">वॉलेट एन्क्रिप्शन विफल हो गया है |</translation>
+    </message>
+    <message>
+        <source>Wallet encryption failed due to an internal error. Your wallet was not encrypted.</source>
+        <translation type="unfinished">आंतरिक त्रुटि के कारण वॉलेट एन्क्रिप्शन विफल हो गया है । आपका वॉलेट एन्क्रिप्ट नहीं किया गया था।</translation>
+    </message>
+    <message>
+        <source>The supplied passphrases do not match.</source>
+        <translation type="unfinished">आपूर्ति किए गए पासफ़्रेज़ मेल नहीं खाते।</translation>
+    </message>
+    <message>
+        <source>Wallet unlock failed</source>
+        <translation type="unfinished">वॉलेट अनलॉक विफल हो गया है |</translation>
+    </message>
+    <message>
+        <source>The passphrase entered for the wallet decryption was incorrect.</source>
+        <translation type="unfinished">वॉलेट डिक्रिप्शन के लिए दर्ज किया गया पासफ़्रेज़ गलत था।</translation>
+    </message>
+    <message>
+        <source>The passphrase entered for the wallet decryption is incorrect. It contains a null character (ie - a zero byte). If the passphrase was set with a version of this software prior to 25.0, please try again with only the characters up to — but not including — the first null character. If this is successful, please set a new passphrase to avoid this issue in the future.</source>
+        <translation type="unfinished">वॉलेट डिक्रिप्शन के लिए दर्ज पासफ़्रेज़ गलत है। इसमें एक अशक्त वर्ण (यानी - एक शून्य बाइट) होता है। यदि पासफ़्रेज़ 25.0 से पहले इस सॉफ़्टवेयर के किसी संस्करण के साथ सेट किया गया था, तो कृपया केवल पहले अशक्त वर्ण तक - लेकिन शामिल नहीं - वर्णों के साथ पुनः प्रयास करें। यदि यह सफल होता है, तो कृपया भविष्य में इस समस्या से बचने के लिए एक नया पासफ़्रेज़ सेट करें।</translation>
+    </message>
+    <message>
+        <source>Passphrase change failed</source>
+        <translation type="unfinished">पदबंध परिवर्तन विफल रहा</translation>
+    </message>
+    <message>
+        <source>The old passphrase entered for the wallet decryption is incorrect. It contains a null character (ie - a zero byte). If the passphrase was set with a version of this software prior to 25.0, please try again with only the characters up to — but not including — the first null character.</source>
+        <translation type="unfinished">वॉलेट डिक्रिप्शन के लिए दर्ज किया गया पुराना पासफ़्रेज़ गलत है। इसमें एक अशक्त वर्ण (यानी - एक शून्य बाइट) होता है। यदि पासफ़्रेज़ 25.0 से पहले इस सॉफ़्टवेयर के किसी संस्करण के साथ सेट किया गया था, तो कृपया केवल पहले अशक्त वर्ण तक - लेकिन शामिल नहीं - वर्णों के साथ पुनः प्रयास करें।</translation>
+    </message>
+    <message>
+        <source>Warning: The Caps Lock key is on!</source>
+        <translation type="unfinished">महत्वपूर्ण सुचना: कैप्स लॉक कुंजी चालू है!</translation>
+    </message>
+</context>
+<context>
+    <name>BanTableModel</name>
+    <message>
+        <source>IP/Netmask</source>
+        <translation type="unfinished">आईपी/नेटमास्क</translation>
+    </message>
+    <message>
+        <source>Banned Until</source>
+        <translation type="unfinished">तक प्रतिबंधित</translation>
+    </message>
+</context>
