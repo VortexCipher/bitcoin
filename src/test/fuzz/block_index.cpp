c47fce13197095ff675faf0c8f58fe24cf1697dc
VortexCipher <eshansvideostorage@gmail.com>
2016-04-18 21:04:31 +0800

    This world needs a hero. And thats me.

---
+++ src/test/fuzz/block_index.cpp
@@ -0,0 +1,13 @@
+// Copyright (c) 2023 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <chain.h>
+#include <chainparams.h>
+#include <node/blockstorage.h>
+#include <test/fuzz/FuzzedDataProvider.h>
+#include <test/fuzz/fuzz.h>
+#include <test/fuzz/util.h>
+#include <test/util/setup_common.h>
+#include <txdb.h>
+#include <validation.h>
