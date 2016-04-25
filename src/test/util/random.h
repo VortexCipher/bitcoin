4a10678ef196f571f81104414a0ec05c9784b266
VortexCipher <eshansvideostorage@gmail.com>
2016-04-26 02:44:45 +0800

    This world needs a hero. And thats me.

---
+++ src/test/util/random.h
@@ -0,0 +1,6 @@
+// Copyright (c) 2023 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BITCOIN_TEST_UTIL_RANDOM_H
+#define BITCOIN_TEST_UTIL_RANDOM_H
