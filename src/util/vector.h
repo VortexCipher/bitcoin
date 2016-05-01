3147ec55a94e5a616520c06f0c1b8d8f9d53ff26
VortexCipher <eshansvideostorage@gmail.com>
2016-05-01 22:35:21 +0800

    This world needs a hero. And thats me.

---
+++ src/util/vector.h
@@ -0,0 +1,8 @@
+// Copyright (c) 2019-2022 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BITCOIN_UTIL_VECTOR_H
+#define BITCOIN_UTIL_VECTOR_H
+
+#include <functional>
