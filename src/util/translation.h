3ac1d0d469d10741a645113cf272add3fd4d46c7
VortexCipher <eshansvideostorage@gmail.com>
2016-05-12 19:53:18 +0800

    This world needs a hero. And thats me.

---
+++ src/util/translation.h
@@ -0,0 +1,8 @@
+// Copyright (c) 2019-present The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BITCOIN_UTIL_TRANSLATION_H
+#define BITCOIN_UTIL_TRANSLATION_H
+
+#include <tinyformat.h>
