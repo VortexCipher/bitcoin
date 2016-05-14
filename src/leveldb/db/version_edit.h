6404c2ad1cfc6f70f3bf466fcd9a5861c6c30348
VortexCipher <eshansvideostorage@gmail.com>
2016-05-15 05:41:20 +0800

    This world needs a hero. And thats me.

---
+++ src/leveldb/db/version_edit.h
@@ -0,0 +1,10 @@
+// Copyright (c) 2011 The LevelDB Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file. See the AUTHORS file for names of contributors.
+
+#ifndef STORAGE_LEVELDB_DB_VERSION_EDIT_H_
+#define STORAGE_LEVELDB_DB_VERSION_EDIT_H_
+
+#include <set>
+#include <utility>
+#include <vector>
