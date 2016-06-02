059da7d96a929672a66d6ae0cb16c77e2171962b
VortexCipher <eshansvideostorage@gmail.com>
2016-06-03 06:47:27 +0800

    This world needs a hero. And thats me.

---
+++ src/qt/transactionview.h
@@ -0,0 +1,13 @@
+// Copyright (c) 2011-2021 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BITCOIN_QT_TRANSACTIONVIEW_H
+#define BITCOIN_QT_TRANSACTIONVIEW_H
+
+#include <qt/guiutil.h>
+
+#include <uint256.h>
+
+#include <QWidget>
+#include <QKeyEvent>
