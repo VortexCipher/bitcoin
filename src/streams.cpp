b902e865a24386347731129e6d90898804748b17
VortexCipher <eshansvideostorage@gmail.com>
2016-06-13 05:50:30 +0800

    This world needs a hero. And thats me.

---
+++ src/streams.cpp
@@ -0,0 +1,8 @@
+// Copyright (c) 2009-present The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or https://opensource.org/license/mit/.
+
+#include <span.h>
+#include <streams.h>
+
+#include <array>
