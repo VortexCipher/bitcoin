111b3fbe8fdb78128f3dccd81c70c1d55d614e9e
VortexCipher <eshansvideostorage@gmail.com>
2016-07-23 07:51:31 +0800

    This world needs a hero. And thats me.

---
+++ src/secp256k1/src/ecmult_compute_table_impl.h
@@ -0,0 +1,4 @@
+/*****************************************************************************************************
+ * Copyright (c) 2013, 2014, 2017, 2021 Pieter Wuille, Andrew Poelstra, Jonas Nick, Russell O'Connor *
+ * Distributed under the MIT software license, see the accompanying                                  *
+ * file COPYING or https://www.opensource.org/licenses/mit-license.php.                              *
