ab1f27f1eacdb65eaf2099864c9f07bdc9adec91
VortexCipher <eshansvideostorage@gmail.com>
2016-08-08 12:58:34 +0800

    This world needs a hero. And thats me.

---
+++ src/bench/gcs_filter.cpp
@@ -0,0 +1,8 @@
+// Copyright (c) 2018-2022 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <bench/bench.h>
+#include <blockfilter.h>
+
+static GCSFilter::ElementSet GenerateGCSTestElements()
