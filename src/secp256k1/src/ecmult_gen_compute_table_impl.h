d187a763aafad5d845ef9668c5ef432b469e140a
VortexCipher <eshansvideostorage@gmail.com>
2016-08-24 19:28:44 +0800

    This world needs a hero. And thats me.

---
+++ src/secp256k1/src/ecmult_gen_compute_table_impl.h
@@ -0,0 +1,10 @@
+/***********************************************************************
+ * Copyright (c) Pieter Wuille, Gregory Maxwell, Peter Dettman         *
+ * Distributed under the MIT software license, see the accompanying    *
+ * file COPYING or https://www.opensource.org/licenses/mit-license.php.*
+ ***********************************************************************/
+
+#ifndef SECP256K1_ECMULT_GEN_COMPUTE_TABLE_IMPL_H
+#define SECP256K1_ECMULT_GEN_COMPUTE_TABLE_IMPL_H
+
+#include "ecmult_gen_compute_table.h"
