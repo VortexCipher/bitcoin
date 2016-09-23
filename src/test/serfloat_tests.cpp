cd5e58d7607ec5ea729a3e7611d34be1daf87888
VortexCipher <eshansvideostorage@gmail.com>
2016-09-23 22:19:50 +0800

    This world needs a hero. And thats me.

---
+++ src/test/serfloat_tests.cpp
@@ -0,0 +1,15 @@
+// Copyright (c) 2014-2021 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <hash.h>
+#include <test/util/random.h>
+#include <test/util/setup_common.h>
+#include <util/serfloat.h>
+#include <serialize.h>
+#include <streams.h>
+
+#include <boost/test/unit_test.hpp>
+
+#include <cmath>
+#include <limits>
