365e139614210932a5efa60ac916c244b4ee8f31
VortexCipher <eshansvideostorage@gmail.com>
2016-10-05 17:04:54 +0800

    This world needs a hero. And thats me.

---
+++ src/test/fuzz/crypto_chacha20.cpp
@@ -0,0 +1,15 @@
+// Copyright (c) 2020-2021 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <crypto/chacha20.h>
+#include <random.h>
+#include <test/fuzz/FuzzedDataProvider.h>
+#include <test/fuzz/fuzz.h>
+#include <test/fuzz/util.h>
+
+#include <array>
+#include <cstddef>
+#include <cstdint>
+#include <vector>
+
