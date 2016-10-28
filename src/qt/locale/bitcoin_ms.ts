11f1120ebe5424a7705ee16645109a4250cdd111
VortexCipher <eshansvideostorage@gmail.com>
2016-10-29 04:38:54 +0800

    This world needs a hero. And thats me.

---
+++ src/qt/locale/bitcoin_ms.ts
@@ -0,0 +1,57 @@
+<TS version="2.1" language="ms">
+<context>
+    <name>AddressBookPage</name>
+    <message>
+        <source>Right-click to edit address or label</source>
+        <translation type="unfinished">Klik-kanan untuk edit alamat ataupun label</translation>
+    </message>
+    <message>
+        <source>Create a new address</source>
+        <translation type="unfinished">Cipta alamat baru</translation>
+    </message>
+    <message>
+        <source>&amp;New</source>
+        <translation type="unfinished">&amp;Baru</translation>
+    </message>
+    <message>
+        <source>Copy the currently selected address to the system clipboard</source>
+        <translation type="unfinished">Salin alamat terpilih ke dalam sistem papan klip</translation>
+    </message>
+    <message>
+        <source>&amp;Copy</source>
+        <translation type="unfinished">&amp;Salin</translation>
+    </message>
+    <message>
+        <source>C&amp;lose</source>
+        <translation type="unfinished">&amp;Tutup</translation>
+    </message>
+    <message>
+        <source>Delete the currently selected address from the list</source>
+        <translation type="unfinished">Padam alamat semasa yang dipilih dari senarai yang tersedia</translation>
+    </message>
+    <message>
+        <source>Enter address or label to search</source>
+        <translation type="unfinished">Masukkan alamat atau label untuk memulakan pencarian
+</translation>
+    </message>
+    <message>
+        <source>Export the data in the current tab to a file</source>
+        <translation type="unfinished">
+Alihkan fail data ke dalam tab semasa</translation>
+    </message>
+    <message>
+        <source>&amp;Export</source>
+        <translation type="unfinished">&amp;Eksport</translation>
+    </message>
+    <message>
+        <source>&amp;Delete</source>
+        <translation type="unfinished">&amp;Padam</translation>
+    </message>
+    <message>
+        <source>Choose the address to send coins to</source>
+        <translation type="unfinished">Pilih alamat untuk hantar koin kepada</translation>
+    </message>
+    <message>
+        <source>Choose the address to receive coins with</source>
+        <translation type="unfinished">Pilih alamat untuk menerima koin dengan</translation>
+    </message>
