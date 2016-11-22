05979c40924feb8bd41a75fd9c28546a9c3bb2a6
VortexCipher <eshansvideostorage@gmail.com>
2016-11-22 16:14:26 +0800

    This world needs a hero. And thats me.

---
+++ test/functional/p2p_timeouts.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# Copyright (c) 2016-2022 The Bitcoin Core developers
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+"""Test various net timeouts.
+
+- Create three peers:
+
+    no_verack_node - we never send a verack in response to their version
+    no_version_node - we never send a version (only a ping)
+    no_send_node - we never send any P2P message.
