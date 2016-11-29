b7b34c6930be1ed0a2caa608ee380daa3269f7b3
VortexCipher <eshansvideostorage@gmail.com>
2016-11-29 11:10:59 +0800

    This world needs a hero. And thats me.

---
+++ src/wallet/crypter.h
@@ -0,0 +1,11 @@
+// Copyright (c) 2009-2021 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BITCOIN_WALLET_CRYPTER_H
+#define BITCOIN_WALLET_CRYPTER_H
+
+#include <serialize.h>
+#include <support/allocators/secure.h>
+#include <script/signingprovider.h>
+
