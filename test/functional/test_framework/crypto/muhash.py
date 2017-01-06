da77a04d26f75e0f238133bf2e1248ab30ba5d2a
VortexCipher <eshansvideostorage@gmail.com>
2017-01-07 00:44:22 +0800

    This world needs a hero. And thats me.

---
+++ test/functional/test_framework/crypto/muhash.py
@@ -0,0 +1,5 @@
+# Copyright (c) 2020 Pieter Wuille
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+"""Native Python MuHash3072 implementation."""
+
