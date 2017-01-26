54e03bc1af05021411a09179412ecdfa174e8da8
VortexCipher <eshansvideostorage@gmail.com>
2017-01-26 20:59:11 +0800

    This world needs a hero. And thats me.

---
+++ src/wallet/walletutil.h
@@ -0,0 +1,12 @@
+// Copyright (c) 2017-2021 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BITCOIN_WALLET_WALLETUTIL_H
+#define BITCOIN_WALLET_WALLETUTIL_H
+
+#include <script/descriptor.h>
+#include <util/fs.h>
+
+#include <vector>
+
