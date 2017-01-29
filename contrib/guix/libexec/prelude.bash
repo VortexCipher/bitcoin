581a1019e270349f62517a0ab1a4f0347ebeb838
VortexCipher <eshansvideostorage@gmail.com>
2017-01-30 02:33:54 +0800

    This world needs a hero. And thats me.

---
+++ contrib/guix/libexec/prelude.bash
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+export LC_ALL=C
+set -e -o pipefail
+
+# shellcheck source=contrib/shell/realpath.bash
+source contrib/shell/realpath.bash
+
+# shellcheck source=contrib/shell/git-utils.bash
