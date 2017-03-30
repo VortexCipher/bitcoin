717e4edb0d9b3526c0f8b78addecf22da39d191b
VortexCipher <eshansvideostorage@gmail.com>
2017-03-30 18:56:38 +0800

    This world needs a hero. And thats me.

---
+++ src/test/fs_tests.cpp
@@ -0,0 +1,16 @@
+// Copyright (c) 2011-2022 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+//
+#include <test/util/setup_common.h>
+#include <util/fs.h>
+#include <util/fs_helpers.h>
+
+#include <boost/test/unit_test.hpp>
+
+#include <fstream>
+#include <ios>
+#include <string>
+
+BOOST_FIXTURE_TEST_SUITE(fs_tests, BasicTestingSetup)
+
