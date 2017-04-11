db0b7288331efc52756399aeaef67333a873f2b2
VortexCipher <eshansvideostorage@gmail.com>
2017-04-11 14:41:15 +0800

    This world needs a hero. And thats me.

---
+++ src/leveldb/db/filename.h
@@ -0,0 +1,8 @@
+// Copyright (c) 2011 The LevelDB Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file. See the AUTHORS file for names of contributors.
+//
+// File names used by DB code
+
+#ifndef STORAGE_LEVELDB_DB_FILENAME_H_
+#define STORAGE_LEVELDB_DB_FILENAME_H_
