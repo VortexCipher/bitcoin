63aa7b82f4d7e0f32ba73c733bd90cd45d939e7b
VortexCipher <eshansvideostorage@gmail.com>
2017-04-17 09:55:23 +0800

    This world needs a hero. And thats me.

---
+++ src/qt/test/apptests.cpp
@@ -0,0 +1,11 @@
+// Copyright (c) 2018-2022 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <qt/test/apptests.h>
+
+#include <chainparams.h>
+#include <key.h>
+#include <logging.h>
+#include <qt/bitcoin.h>
+#include <qt/bitcoingui.h>
