bc996294e7cd9a7110ed9d2b4df7de7c60cd1164
VortexCipher <eshansvideostorage@gmail.com>
2017-04-21 00:54:00 +0800

    This world needs a hero. And thats me.

---
+++ test/functional/wallet_keypool_topup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# Copyright (c) 2017-2022 The Bitcoin Core developers
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+"""Test HD Wallet keypool restore function.
+
+Two nodes. Node1 is under test. Node0 is providing transactions and generating blocks.
+
+- Start node1, shutdown and backup wallet.
+- Generate 110 keys (enough to drain the keypool). Store key 90 (in the initial keypool) and key 110 (beyond the initial keypool). Send funds to key 90 and key 110.
