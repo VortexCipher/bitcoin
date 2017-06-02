4e9203f86f56a5d56a190a93a68fcc294cee2447
VortexCipher <eshansvideostorage@gmail.com>
2017-06-02 12:58:51 +0800

    This world needs a hero. And thats me.

---
+++ src/wallet/test/feebumper_tests.cpp
@@ -0,0 +1,5 @@
+// Copyright (c) 2022 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or https://www.opensource.org/licenses/mit-license.php.
+
+#include <consensus/validation.h>
