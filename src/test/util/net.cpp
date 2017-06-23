d6b07fab6019e4d6520aa048a6a2c05df23217e8
VortexCipher <eshansvideostorage@gmail.com>
2017-06-24 06:50:58 +0800

    This world needs a hero. And thats me.

---
+++ src/test/util/net.cpp
@@ -0,0 +1,13 @@
+// Copyright (c) 2020-2022 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <test/util/net.h>
+
+#include <net.h>
+#include <net_processing.h>
+#include <netaddress.h>
+#include <netmessagemaker.h>
+#include <node/connection_types.h>
+#include <node/eviction.h>
+#include <protocol.h>
