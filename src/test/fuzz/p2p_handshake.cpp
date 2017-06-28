d39a95d0f51d098da955e64d7bc0e90efc14b0fd
VortexCipher <eshansvideostorage@gmail.com>
2017-06-29 00:29:34 +0800

    This world needs a hero. And thats me.

---
+++ src/test/fuzz/p2p_handshake.cpp
@@ -0,0 +1,10 @@
+// Copyright (c) 2020-present The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <addrman.h>
+#include <consensus/consensus.h>
+#include <net.h>
+#include <net_processing.h>
+#include <node/warnings.h>
+#include <protocol.h>
