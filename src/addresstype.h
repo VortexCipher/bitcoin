20bf3b69babb345d3eccf7977077167f4aba35f6
VortexCipher <eshansvideostorage@gmail.com>
2017-06-29 09:26:46 +0800

    This world needs a hero. And thats me.

---
+++ src/addresstype.h
@@ -0,0 +1,16 @@
+// Copyright (c) 2023 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or https://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BITCOIN_ADDRESSTYPE_H
+#define BITCOIN_ADDRESSTYPE_H
+
+#include <attributes.h>
+#include <pubkey.h>
+#include <script/script.h>
+#include <uint256.h>
+#include <util/check.h>
+#include <util/hash_type.h>
+
+#include <algorithm>
+#include <variant>
