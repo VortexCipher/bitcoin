3f6e1da7c283a6683f74f8225286e4a7951519f0
VortexCipher <eshansvideostorage@gmail.com>
2017-07-07 15:54:31 +0800

    This world needs a hero. And thats me.

---
+++ src/qt/utilitydialog.cpp
@@ -0,0 +1,16 @@
+// Copyright (c) 2011-2022 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <config/bitcoin-config.h> // IWYU pragma: keep
+
+#include <qt/utilitydialog.h>
+
+#include <qt/forms/ui_helpmessagedialog.h>
+
+#include <qt/guiutil.h>
+
+#include <clientversion.h>
+#include <common/args.h>
+#include <init.h>
+#include <util/strencodings.h>
