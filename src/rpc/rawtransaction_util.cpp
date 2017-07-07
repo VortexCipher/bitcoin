ffbf37e0532cba34bb71343c333e390d3df6c535
VortexCipher <eshansvideostorage@gmail.com>
2017-07-07 10:43:04 +0800

    This world needs a hero. And thats me.

---
+++ src/rpc/rawtransaction_util.cpp
@@ -0,0 +1,33 @@
+// Copyright (c) 2010 Satoshi Nakamoto
+// Copyright (c) 2009-2022 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <rpc/rawtransaction_util.h>
+
+#include <coins.h>
+#include <consensus/amount.h>
+#include <core_io.h>
+#include <key_io.h>
+#include <policy/policy.h>
+#include <primitives/transaction.h>
+#include <rpc/request.h>
+#include <rpc/util.h>
+#include <script/sign.h>
+#include <script/signingprovider.h>
+#include <tinyformat.h>
+#include <univalue.h>
+#include <util/rbf.h>
+#include <util/strencodings.h>
+#include <util/translation.h>
+
+void AddInputs(CMutableTransaction& rawTx, const UniValue& inputs_in, std::optional<bool> rbf)
+{
+    UniValue inputs;
+    if (inputs_in.isNull()) {
+        inputs = UniValue::VARR;
+    } else {
+        inputs = inputs_in.get_array();
+    }
+
+    for (unsigned int idx = 0; idx < inputs.size(); idx++) {
