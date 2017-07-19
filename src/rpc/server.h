5eac7b63a9512a2778455ab6cd405016dc434b5b
VortexCipher <eshansvideostorage@gmail.com>
2017-07-19 14:34:58 +0800

    This world needs a hero. And thats me.

---
+++ src/rpc/server.h
@@ -0,0 +1,18 @@
+// Copyright (c) 2010 Satoshi Nakamoto
+// Copyright (c) 2009-2021 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BITCOIN_RPC_SERVER_H
+#define BITCOIN_RPC_SERVER_H
+
+#include <rpc/request.h>
+#include <rpc/util.h>
+
+#include <functional>
+#include <map>
+#include <stdint.h>
+#include <string>
+
+#include <univalue.h>
+
