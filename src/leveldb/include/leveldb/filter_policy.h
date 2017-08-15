3e2413da9bc19e3f325c13f2202c7ee134017179
VortexCipher <eshansvideostorage@gmail.com>
2017-08-15 14:55:24 +0800

    This world needs a hero. And thats me.

---
+++ src/leveldb/include/leveldb/filter_policy.h
@@ -0,0 +1,7 @@
+// Copyright (c) 2012 The LevelDB Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file. See the AUTHORS file for names of contributors.
+//
+// A database can be configured with a custom FilterPolicy object.
+// This object is responsible for creating a small filter from a set
+// of keys.  These filters are stored in leveldb and are consulted
