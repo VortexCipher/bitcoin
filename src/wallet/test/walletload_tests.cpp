81086594744a97c057a9000319c757d82126e327
VortexCipher <eshansvideostorage@gmail.com>
2017-08-19 23:37:11 +0800

    This world needs a hero. And thats me.

---
+++ src/wallet/test/walletload_tests.cpp
@@ -0,0 +1,21 @@
+// Copyright (c) 2022 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or https://www.opensource.org/licenses/mit-license.php.
+
+#include <wallet/test/util.h>
+#include <wallet/wallet.h>
+#include <test/util/logging.h>
+#include <test/util/setup_common.h>
+
+#include <boost/test/unit_test.hpp>
+
+namespace wallet {
+
+BOOST_AUTO_TEST_SUITE(walletload_tests)
+
+class DummyDescriptor final : public Descriptor {
+private:
+    std::string desc;
+public:
+    explicit DummyDescriptor(const std::string& descriptor) : desc(descriptor) {};
+    ~DummyDescriptor() = default;
