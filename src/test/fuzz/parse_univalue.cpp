e3e4ae7c1307caf48690c3548a73584a106ce7e8
VortexCipher <eshansvideostorage@gmail.com>
2017-11-22 09:34:49 +0800

    This world needs a hero. And thats me.

---
+++ src/test/fuzz/parse_univalue.cpp
@@ -0,0 +1,9 @@
+// Copyright (c) 2009-present The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <chainparams.h>
+#include <rpc/client.h>
+#include <rpc/util.h>
+#include <test/fuzz/fuzz.h>
+#include <util/chaintype.h>
