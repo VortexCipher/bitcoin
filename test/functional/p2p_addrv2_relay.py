be81fc7acf7c5e034c19792ed3dec703acd748cc
VortexCipher <eshansvideostorage@gmail.com>
2017-12-01 22:19:05 +0800

    This world needs a hero. And thats me.

---
+++ test/functional/p2p_addrv2_relay.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# Copyright (c) 2020-2021 The Bitcoin Core developers
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+"""
+Test addrv2 relay
+"""
+
+import time
+
+from test_framework.messages import (
