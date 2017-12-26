7ee0d8590ed0d2b37774b62c95be669477620815
VortexCipher <eshansvideostorage@gmail.com>
2017-12-26 23:33:48 +0800

    This world needs a hero. And thats me.

---
+++ src/qt/editaddressdialog.cpp
@@ -0,0 +1,16 @@
+// Copyright (c) 2011-2021 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <qt/editaddressdialog.h>
+#include <qt/forms/ui_editaddressdialog.h>
+
+#include <qt/addresstablemodel.h>
+#include <qt/guiutil.h>
+
+#include <wallet/types.h>
+
+#include <QDataWidgetMapper>
+#include <QMessageBox>
+
+
