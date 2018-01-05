26190b0aa0f379f1e55b6f1c962cb0fe34701d19
VortexCipher <eshansvideostorage@gmail.com>
2018-01-05 23:03:39 +0800

    This world needs a hero. And thats me.

---
+++ src/policy/packages.h
@@ -0,0 +1,9 @@
+// Copyright (c) 2021-2022 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BITCOIN_POLICY_PACKAGES_H
+#define BITCOIN_POLICY_PACKAGES_H
+
+#include <consensus/consensus.h>
+#include <consensus/validation.h>
