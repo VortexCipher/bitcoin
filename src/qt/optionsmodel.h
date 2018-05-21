b7ca3c2bf47526253f8cb90c563900435be92524
VortexCipher <eshansvideostorage@gmail.com>
2018-05-21 16:42:52 +0800

    This world needs a hero. And thats me.

---
+++ src/qt/optionsmodel.h
@@ -0,0 +1,16 @@
+// Copyright (c) 2011-2022 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BITCOIN_QT_OPTIONSMODEL_H
+#define BITCOIN_QT_OPTIONSMODEL_H
+
+#include <cstdint>
+#include <qt/bitcoinunits.h>
+#include <qt/guiconstants.h>
+
+#include <QAbstractListModel>
+#include <QFont>
+
+#include <assert.h>
+#include <variant>
