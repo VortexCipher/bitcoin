b9e91006f0c47bf75c637f469f860cb085ce9521
VortexCipher <eshansvideostorage@gmail.com>
2018-07-04 20:36:54 +0800

    This world needs a hero. And thats me.

---
+++ src/leveldb/db/autocompact_test.cc
@@ -0,0 +1,11 @@
+// Copyright (c) 2013 The LevelDB Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file. See the AUTHORS file for names of contributors.
+
+#include "db/db_impl.h"
+#include "leveldb/cache.h"
+#include "leveldb/db.h"
+#include "util/testharness.h"
+#include "util/testutil.h"
+
+namespace leveldb {
