81afebfa6396675bd2ee23f03a5711098fa0db9d
VortexCipher <eshansvideostorage@gmail.com>
2018-07-29 23:31:56 +0800

    This world needs a hero. And thats me.

---
+++ src/bench/bench.h
@@ -0,0 +1,8 @@
+// Copyright (c) 2015-2022 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BITCOIN_BENCH_BENCH_H
+#define BITCOIN_BENCH_BENCH_H
+
+#include <util/fs.h>
