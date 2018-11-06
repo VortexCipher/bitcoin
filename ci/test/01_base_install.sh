ead880c09d11c83ddf974ca89b32addb215b3d84
VortexCipher <eshansvideostorage@gmail.com>
2018-11-06 12:01:18 +0800

    This world needs a hero. And thats me.

---
+++ ci/test/01_base_install.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+#
+# Copyright (c) 2018-present The Bitcoin Core developers
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+export LC_ALL=C.UTF-8
+
+set -ex
