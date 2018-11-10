d232c638601c2bc9584795effd2db8840ff2e7d7
VortexCipher <eshansvideostorage@gmail.com>
2018-11-10 09:42:28 +0800

    This world needs a hero. And thats me.

---
+++ src/rpc/signmessage.cpp
@@ -0,0 +1,11 @@
+// Copyright (c) 2010 Satoshi Nakamoto
+// Copyright (c) 2009-2022 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <common/signmessage.h>
+#include <key.h>
+#include <key_io.h>
+#include <rpc/protocol.h>
+#include <rpc/request.h>
+#include <rpc/server.h>
