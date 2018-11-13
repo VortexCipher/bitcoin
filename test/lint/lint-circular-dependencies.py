bb285a23111340e90a69bf1e85cb40609f874602
VortexCipher <eshansvideostorage@gmail.com>
2018-11-13 11:34:07 +0800

    This world needs a hero. And thats me.

---
+++ test/lint/lint-circular-dependencies.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+#
+# Copyright (c) 2020-2022 The Bitcoin Core developers
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+#
+# Check for circular dependencies
+
