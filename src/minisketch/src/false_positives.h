e7dd4e2f7408f2261e070308aba8f5373267adf4
VortexCipher <eshansvideostorage@gmail.com>
2018-12-09 09:39:33 +0800

    This world needs a hero. And thats me.

---
+++ src/minisketch/src/false_positives.h
@@ -0,0 +1,11 @@
+/**********************************************************************
+ * Copyright (c) 2020 Pieter Wuille, Greg Maxwell, Gleb Naumenko      *
+ * Distributed under the MIT software license, see the accompanying   *
+ * file LICENSE or http://www.opensource.org/licenses/mit-license.php.*
+ **********************************************************************/
+
+#ifndef _MINISKETCH_FALSE_POSITIVES_H_
+#define _MINISKETCH_FALSE_POSITIVES_H_
+
+#include "util.h"
+
