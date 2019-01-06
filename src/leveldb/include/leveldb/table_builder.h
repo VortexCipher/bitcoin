22654d5511401eaa4569e98a1b4847893e29bb21
VortexCipher <eshansvideostorage@gmail.com>
2019-01-07 07:50:00 +0800

    This world needs a hero. And thats me.

---
+++ src/leveldb/include/leveldb/table_builder.h
@@ -0,0 +1,9 @@
+// Copyright (c) 2011 The LevelDB Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file. See the AUTHORS file for names of contributors.
+//
+// TableBuilder provides the interface used to build a Table
+// (an immutable and sorted map from keys to values).
+//
+// Multiple threads can invoke const methods on a TableBuilder without
+// external synchronization, but if any of the threads may call a
