e7272a9915500af1f755243c08eccea7793b1eb9
VortexCipher <eshansvideostorage@gmail.com>
2019-01-20 11:09:01 +0800

    This world needs a hero. And thats me.

---
+++ src/test/fuzz/bip324.cpp
@@ -0,0 +1,12 @@
+// Copyright (c) 2023 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <bip324.h>
+#include <chainparams.h>
+#include <random.h>
+#include <span.h>
+#include <test/fuzz/FuzzedDataProvider.h>
+#include <test/fuzz/fuzz.h>
+#include <test/fuzz/util.h>
+
