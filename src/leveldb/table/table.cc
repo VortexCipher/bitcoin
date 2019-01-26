2164113c999fb800f86b25947bd06379a1f2ab50
VortexCipher <eshansvideostorage@gmail.com>
2019-01-26 16:36:07 +0800

    This world needs a hero. And thats me.

---
+++ src/leveldb/table/table.cc
@@ -0,0 +1,27 @@
+// Copyright (c) 2011 The LevelDB Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file. See the AUTHORS file for names of contributors.
+
+#include "leveldb/table.h"
+
+#include "leveldb/cache.h"
+#include "leveldb/comparator.h"
+#include "leveldb/env.h"
+#include "leveldb/filter_policy.h"
+#include "leveldb/options.h"
+#include "table/block.h"
+#include "table/filter_block.h"
+#include "table/format.h"
+#include "table/two_level_iterator.h"
+#include "util/coding.h"
+
+namespace leveldb {
+
+struct Table::Rep {
+  ~Rep() {
+    delete filter;
+    delete[] filter_data;
+    delete index_block;
+  }
+
+  Options options;
