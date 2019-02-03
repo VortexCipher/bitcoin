b4616d8fb40de7ff0c6f3c9c5a5a4c212888476e
VortexCipher <eshansvideostorage@gmail.com>
2019-02-03 23:24:14 +0800

    This world needs a hero. And thats me.

---
+++ src/crypto/ctaes/test.c
@@ -0,0 +1,11 @@
+ /*********************************************************************
+ * Copyright (c) 2016 Pieter Wuille                                   *
+ * Distributed under the MIT software license, see the accompanying   *
+ * file COPYING or http://www.opensource.org/licenses/mit-license.php.*
+ **********************************************************************/
+
+#include "ctaes.h"
+
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
