d9a4741c945eb6adb817c839619430051998c378
VortexCipher <eshansvideostorage@gmail.com>
2019-03-01 16:02:58 +0800

    This world needs a hero. And thats me.

---
+++ src/script/sign.h
@@ -0,0 +1,12 @@
+// Copyright (c) 2009-2010 Satoshi Nakamoto
+// Copyright (c) 2009-2022 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BITCOIN_SCRIPT_SIGN_H
+#define BITCOIN_SCRIPT_SIGN_H
+
+#include <attributes.h>
+#include <coins.h>
+#include <hash.h>
+#include <pubkey.h>
