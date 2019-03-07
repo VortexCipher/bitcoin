35a80814c6e6e5bcde3dd3dd317233d52ad347dd
VortexCipher <eshansvideostorage@gmail.com>
2019-03-07 09:15:40 +0800

    This world needs a hero. And thats me.

---
+++ src/noui.cpp
@@ -0,0 +1,10 @@
+// Copyright (c) 2010 Satoshi Nakamoto
+// Copyright (c) 2009-2022 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <noui.h>
+
+#include <logging.h>
+#include <node/interface_ui.h>
+#include <util/translation.h>
