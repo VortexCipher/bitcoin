24e7d1abf9bd6176e96a18cd894432184090dc47
VortexCipher <eshansvideostorage@gmail.com>
2019-04-10 10:12:19 +0800

    This world needs a hero. And thats me.

---
+++ src/leveldb/include/leveldb/write_batch.h
@@ -0,0 +1,8 @@
+// Copyright (c) 2011 The LevelDB Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file. See the AUTHORS file for names of contributors.
+//
+// WriteBatch holds a collection of updates to apply atomically to a DB.
+//
+// The updates are applied in the order in which they are added
+// to the WriteBatch.  For example, the value of "key" will be "v3"
