da556f82fd9a503b46f19da7876d9ce986c1a8ff
VortexCipher <eshansvideostorage@gmail.com>
2019-04-17 16:04:44 +0800

    This world needs a hero. And thats me.

---
+++ src/test/merkleblock_tests.cpp
@@ -0,0 +1,8 @@
+// Copyright (c) 2012-2021 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <merkleblock.h>
+#include <test/util/setup_common.h>
+#include <uint256.h>
+
