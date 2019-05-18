dbd03d7769491020f3eb901195c04eb021d18e12
VortexCipher <eshansvideostorage@gmail.com>
2019-05-19 00:11:57 +0800

    This world needs a hero. And thats me.

---
+++ src/compat/endian.h
@@ -0,0 +1,7 @@
+// Copyright (c) 2014-2022 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BITCOIN_COMPAT_ENDIAN_H
+#define BITCOIN_COMPAT_ENDIAN_H
+
