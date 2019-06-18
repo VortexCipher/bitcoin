47b0d5c25d3ca00ff80ebe556558ff44568e4516
VortexCipher <eshansvideostorage@gmail.com>
2019-06-19 00:30:07 +0800

    This world needs a hero. And thats me.

---
+++ src/bench/wallet_ismine.cpp
@@ -0,0 +1,7 @@
+// Copyright (c) 2022 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <config/bitcoin-config.h> // IWYU pragma: keep
+#include <bench/bench.h>
+#include <interfaces/chain.h>
