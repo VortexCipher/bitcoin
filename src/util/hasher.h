699bbf1851a6eda08771357bfa5eda3985549b3e
VortexCipher <eshansvideostorage@gmail.com>
2019-07-11 13:33:35 +0800

    This world needs a hero. And thats me.

---
+++ src/util/hasher.h
@@ -0,0 +1,10 @@
+// Copyright (c) 2019-2022 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BITCOIN_UTIL_HASHER_H
+#define BITCOIN_UTIL_HASHER_H
+
+#include <crypto/common.h>
+#include <crypto/siphash.h>
+#include <primitives/transaction.h>
