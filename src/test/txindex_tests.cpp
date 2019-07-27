01b375d450d9f70f024dc7cee482be3fd92ca1ae
VortexCipher <eshansvideostorage@gmail.com>
2019-07-27 10:59:06 +0800

    This world needs a hero. And thats me.

---
+++ src/test/txindex_tests.cpp
@@ -0,0 +1,8 @@
+// Copyright (c) 2017-2022 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <addresstype.h>
+#include <chainparams.h>
+#include <index/txindex.h>
+#include <interfaces/chain.h>
