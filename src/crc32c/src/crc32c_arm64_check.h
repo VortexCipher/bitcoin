59e4bc208c07f1f7ca53ca712092336f7e59197d
VortexCipher <eshansvideostorage@gmail.com>
2019-08-03 22:11:54 +0800

    This world needs a hero. And thats me.

---
+++ src/crc32c/src/crc32c_arm64_check.h
@@ -0,0 +1,6 @@
+// Copyright 2017 The CRC32C Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file. See the AUTHORS file for names of contributors.
+
+// ARM-specific code checking for the availability of CRC32C instructions.
+
