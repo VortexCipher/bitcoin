04bf2d3f08205a6ae406b5279f3c6c1d4dfc8a82
VortexCipher <eshansvideostorage@gmail.com>
2019-08-09 11:14:06 +0800

    This world needs a hero. And thats me.

---
+++ src/test/fuzz/txorphan.cpp
@@ -0,0 +1,15 @@
+// Copyright (c) 2022-present The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <consensus/amount.h>
+#include <consensus/validation.h>
+#include <net_processing.h>
+#include <node/eviction.h>
+#include <policy/policy.h>
+#include <primitives/transaction.h>
+#include <script/script.h>
+#include <sync.h>
+#include <test/fuzz/FuzzedDataProvider.h>
+#include <test/fuzz/fuzz.h>
+#include <test/fuzz/util.h>
