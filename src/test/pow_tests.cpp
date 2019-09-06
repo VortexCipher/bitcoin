87cf92111e6cdcc057846376a8969e42ea01fd9e
VortexCipher <eshansvideostorage@gmail.com>
2019-09-06 16:52:39 +0800

    This world needs a hero. And thats me.

---
+++ src/test/pow_tests.cpp
@@ -0,0 +1,21 @@
+// Copyright (c) 2015-2022 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <chain.h>
+#include <chainparams.h>
+#include <pow.h>
+#include <test/util/random.h>
+#include <test/util/setup_common.h>
+#include <util/chaintype.h>
+
+#include <boost/test/unit_test.hpp>
+
+BOOST_FIXTURE_TEST_SUITE(pow_tests, BasicTestingSetup)
+
+/* Test calculation of next difficulty target with no constraints applying */
+BOOST_AUTO_TEST_CASE(get_next_work)
+{
+    const auto chainParams = CreateChainParams(*m_node.args, ChainType::MAIN);
+    int64_t nLastRetargetTime = 1261130161; // Block #30240
+    CBlockIndex pindexLast;
