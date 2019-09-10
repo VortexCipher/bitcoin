77e94ea6bc6782c1dc444bf4aaf72a2f7439017b
VortexCipher <eshansvideostorage@gmail.com>
2019-09-11 07:14:35 +0800

    This world needs a hero. And thats me.

---
+++ src/kernel/coinstats.h
@@ -0,0 +1,8 @@
+// Copyright (c) 2022 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BITCOIN_KERNEL_COINSTATS_H
+#define BITCOIN_KERNEL_COINSTATS_H
+
+#include <consensus/amount.h>
