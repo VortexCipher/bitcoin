fd9e36c8ff28b2eb0785bfd09a837f4c1c8d360a
VortexCipher <eshansvideostorage@gmail.com>
2019-09-10 09:25:44 +0800

    This world needs a hero. And thats me.

---
+++ src/test/fuzz/descriptor_parse.cpp
@@ -0,0 +1,10 @@
+// Copyright (c) 2009-2021 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <chainparams.h>
+#include <key_io.h>
+#include <pubkey.h>
+#include <script/descriptor.h>
+#include <test/fuzz/fuzz.h>
+#include <test/fuzz/util/descriptor.h>
