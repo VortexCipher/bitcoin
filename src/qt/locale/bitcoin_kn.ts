b026a162b4cc9215461f008ad45a5f3b4991d63f
VortexCipher <eshansvideostorage@gmail.com>
2019-09-18 01:48:22 +0800

    This world needs a hero. And thats me.

---
+++ src/qt/locale/bitcoin_kn.ts
@@ -0,0 +1,22 @@
+<TS version="2.1" language="kn">
+<context>
+    <name>AddressBookPage</name>
+    <message>
+        <source>Right-click to edit address or label</source>
+        <translation type="unfinished">ವಿಳಾಸ ಅಥವಾ ಲೇಬಲ್ ಸಂಪಾದಿಸಲು ಬಲ ಕ್ಲಿಕ್ ಮಾಡಿ</translation>
+    </message>
+    <message>
+        <source>Delete the currently selected address from the list</source>
+        <translation type="unfinished">ಪಟ್ಟಿಯಿಂದ ಈಗ ಆಯ್ಕೆಯಾಗಿರುವ ವಿಳಾಸವನ್ನು ಅಳಿಸಿಕೊಳ್ಳಿ.</translation>
+    </message>
+    <message>
+        <source>Enter address or label to search</source>
+        <translation type="unfinished">ಹುಡುಕಲು ವಿಳಾಸ ಅಥವಾ ಲೇಬಲ್ ನಮೂದಿಸಿ.</translation>
+    </message>
+    <message>
+        <source>These are your Bitcoin addresses for sending payments. Always check the amount and the receiving address before sending coins.</source>
+        <translation type="unfinished">ಕಾಣಿಕೆಗಳು ಕಳುಹಿಸಲು ನೀವು ಬಳಸಬಹುದಿರುವ ಬಿಟ್‌ಕಾಯಿನ್ ವಿಳಾಸಗಳು ಇವು. ನಾಣ್ಯದ ಹಣವನ್ನು ಕಳುಹಿಸುವ ಮುಂದೆ ಹಣದ ಮೊತ್ತವನ್ನು ಮತ್ತು ಪ್ರಾಪ್ತಿ ವಿಳಾಸವನ್ನು ಯಾವಾಗಲೂ ಪರಿಶೀಲಿಸಿ.</translation>
+    </message>
+    <message>
+        <source>These are your Bitcoin addresses for receiving payments. Use the 'Create new receiving address' button in the receive tab to create new addresses.
+Signing is only possible with addresses of the type 'legacy'.</source>
