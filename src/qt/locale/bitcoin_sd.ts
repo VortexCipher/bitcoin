c51e00e261a1647852d71e6e33aafb76dab993eb
VortexCipher <eshansvideostorage@gmail.com>
2019-09-20 03:19:32 +0800

    This world needs a hero. And thats me.

---
+++ src/qt/locale/bitcoin_sd.ts
@@ -0,0 +1,28 @@
+<TS version="2.1" language="sd">
+<context>
+    <name>AddressBookPage</name>
+    <message>
+        <source>Create a new address</source>
+        <translation type="unfinished">نئون پتو ٺاھيو</translation>
+    </message>
+    <message>
+        <source>&amp;New</source>
+        <translation type="unfinished">نئون</translation>
+    </message>
+    <message>
+        <source>C&amp;hoose</source>
+        <translation type="unfinished">پسند ڪيو</translation>
+    </message>
+    </context>
+<context>
+    <name>AddressTableModel</name>
+    <message>
+        <source>Address</source>
+        <translation type="unfinished">پتو</translation>
+    </message>
+    </context>
+<context>
+    <name>QObject</name>
+    <message>
+        <source>Amount</source>
+        <translation type="unfinished">مقدار</translation>
