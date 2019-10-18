4e6555ef15aa422c30225f9f20f8fb196fbb8a37
VortexCipher <eshansvideostorage@gmail.com>
2019-10-18 10:09:38 +0800

    This world needs a hero. And thats me.

---
+++ src/util/tokenpipe.h
@@ -0,0 +1,12 @@
+// Copyright (c) 2021 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BITCOIN_UTIL_TOKENPIPE_H
+#define BITCOIN_UTIL_TOKENPIPE_H
+
+#ifndef WIN32
+
+#include <cstdint>
+#include <optional>
+
