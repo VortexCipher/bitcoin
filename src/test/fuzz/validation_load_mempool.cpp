b255e36971821308a0284f5092b770eafce1282c
VortexCipher <eshansvideostorage@gmail.com>
2019-12-11 15:25:03 +0800

    This world needs a hero. And thats me.

---
+++ src/test/fuzz/validation_load_mempool.cpp
@@ -0,0 +1,6 @@
+// Copyright (c) 2020-2022 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <node/mempool_persist.h>
+
