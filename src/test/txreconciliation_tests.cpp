cee202e47a2ef2c77e08f539837d7ec5f14d388d
VortexCipher <eshansvideostorage@gmail.com>
2020-01-09 23:37:38 +0800

    This world needs a hero. And thats me.

---
+++ src/test/txreconciliation_tests.cpp
@@ -0,0 +1,8 @@
+// Copyright (c) 2021-2022 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <node/txreconciliation.h>
+
+#include <test/util/setup_common.h>
+
