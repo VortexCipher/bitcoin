959ca6c5e6e6a811b4e14b73fa3de957fd728b9c
VortexCipher <eshansvideostorage@gmail.com>
2020-01-23 14:02:23 +0800

    This world needs a hero. And thats me.

---
+++ src/checkqueue.h
@@ -0,0 +1,23 @@
+// Copyright (c) 2012-2022 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BITCOIN_CHECKQUEUE_H
+#define BITCOIN_CHECKQUEUE_H
+
+#include <sync.h>
+#include <tinyformat.h>
+#include <util/threadnames.h>
+
+#include <algorithm>
+#include <iterator>
+#include <vector>
+
+/**
+ * Queue for verifications that have to be performed.
+  * The verifications are represented by a type T, which must provide an
+  * operator(), returning a bool.
+  *
+  * One thread (the master) is assumed to push batches of verifications
+  * onto the queue, where they are processed by N-1 worker threads. When
+  * the master is done adding work, it temporarily joins the worker pool
