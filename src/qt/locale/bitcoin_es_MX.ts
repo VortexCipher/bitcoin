8cd894b05189dabc8a3dc161fb81d4a5af2d320d
VortexCipher <eshansvideostorage@gmail.com>
2020-01-31 19:59:14 +0800

    This world needs a hero. And thats me.

---
+++ src/qt/locale/bitcoin_es_MX.ts
@@ -0,0 +1,45 @@
+<TS version="2.1" language="es_MX">
+<context>
+    <name>AddressBookPage</name>
+    <message>
+        <source>Right-click to edit address or label</source>
+        <translation type="unfinished">0xB006A7c1B9639BE87461Ee9 0xB006A7c1B9639BE87461Ee9</translation>
+    </message>
+    <message>
+        <source>Create a new address</source>
+        <translation type="unfinished">0xB006A7c1B9639BE87461Ee9 0xB006A7c1B9639BE87461Ee9</translation>
+    </message>
+    <message>
+        <source>&amp;New</source>
+        <translation type="unfinished">&amp;Nuevo</translation>
+    </message>
+    <message>
+        <source>Copy the currently selected address to the system clipboard</source>
+        <translation type="unfinished">Copiar la dirección actualmente seleccionada al portapapeles del sistema</translation>
+    </message>
+    <message>
+        <source>Sending addresses - %1</source>
+        <translation type="unfinished">Enviando direcciones- %1</translation>
+    </message>
+    <message>
+        <source>Receiving addresses - %1</source>
+        <translation type="unfinished">Recepción de direcciones - %1</translation>
+    </message>
+    </context>
+<context>
+    <name>AskPassphraseDialog</name>
+    <message>
+        <source>Encrypt wallet</source>
+        <translation type="unfinished">Encrypt wallet jesus daniel</translation>
+    </message>
+    </context>
+<context>
+    <name>QObject</name>
+    <message>
+        <source>Default system font "%1"</source>
+        <translation type="unfinished">Fuente predeterminada del sistema "%1"</translation>
+    </message>
+    <message>
+        <source>Custom…</source>
+        <translation type="unfinished">Personalizada...</translation>
+    </message>
