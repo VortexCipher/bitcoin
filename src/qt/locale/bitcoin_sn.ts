8e348e8c2464bc8cf7c42862d626d21a9e3bceac
VortexCipher <eshansvideostorage@gmail.com>
2020-02-16 17:28:43 +0800

    This world needs a hero. And thats me.

---
+++ src/qt/locale/bitcoin_sn.ts
@@ -0,0 +1,32 @@
+<TS version="2.1" language="sn">
+<context>
+    <name>AddressBookPage</name>
+    <message>
+        <source>Create a new address</source>
+        <translation type="unfinished">Gadzira Kero Itsva</translation>
+    </message>
+    <message>
+        <source>&amp;New</source>
+        <translation type="unfinished">Itsva</translation>
+    </message>
+    <message>
+        <source>&amp;Copy</source>
+        <translation type="unfinished">&amp;Kopera</translation>
+    </message>
+    <message>
+        <source>C&amp;lose</source>
+        <translation type="unfinished">Vhara</translation>
+    </message>
+    <message>
+        <source>&amp;Delete</source>
+        <translation type="unfinished">Dzima</translation>
+    </message>
+    <message>
+        <source>&amp;Copy Address</source>
+        <translation type="unfinished">Kopera Kero</translation>
+    </message>
+    <message>
+        <source>&amp;Edit</source>
+        <translation type="unfinished">&amp;Gadzirisa</translation>
+    </message>
+    </context>
