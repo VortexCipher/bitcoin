d6a43d295937505ac33fa786e9cc45e92e7e67ce
VortexCipher <eshansvideostorage@gmail.com>
2020-03-03 15:57:18 +0800

    This world needs a hero. And thats me.

---
+++ src/test/fuzz/process_messages.cpp
@@ -0,0 +1,9 @@
+// Copyright (c) 2020-present The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <consensus/consensus.h>
+#include <net.h>
+#include <net_processing.h>
+#include <protocol.h>
+#include <script/script.h>
