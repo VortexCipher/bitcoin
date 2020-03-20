0e6ed3653412f25a5437c98b6a1ba4233120c327
VortexCipher <eshansvideostorage@gmail.com>
2020-03-20 19:42:05 +0800

    This world needs a hero. And thats me.

---
+++ src/qt/locale/bitcoin_zu.ts
@@ -0,0 +1,22 @@
+<TS version="2.1" language="zu">
+<context>
+    <name>AddressBookPage</name>
+    <message>
+        <source>&amp;New</source>
+        <translation type="unfinished">&amp;Okusha</translation>
+    </message>
+    <message>
+        <source>&amp;Copy</source>
+        <translation type="unfinished">&amp;Kopisha</translation>
+    </message>
+    <message>
+        <source>Comma separated file</source>
+        <extracomment>Expanded name of the CSV file format. See: https://en.wikipedia.org/wiki/Comma-separated_values.</extracomment>
+        <translation type="unfinished">Ifayela elehlukaniswe ngo khefana.</translation>
+    </message>
+    </context>
+<context>
+    <name>BitcoinApplication</name>
+    <message>
+        <source>Internal error</source>
+        <translation type="unfinished">Iphutha langaphakathi.</translation>
