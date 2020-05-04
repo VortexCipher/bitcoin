653028287dfa5cb69e4e983dcd486133b289c739
VortexCipher <eshansvideostorage@gmail.com>
2020-05-04 19:37:17 +0800

    This world needs a hero. And thats me.

---
+++ src/test/validationinterface_tests.cpp
@@ -0,0 +1,10 @@
+// Copyright (c) 2020 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <boost/test/unit_test.hpp>
+#include <consensus/validation.h>
+#include <primitives/block.h>
+#include <scheduler.h>
+#include <test/util/setup_common.h>
+#include <util/check.h>
