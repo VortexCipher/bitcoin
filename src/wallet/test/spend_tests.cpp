d7ead39ee41069203762e46e9961af674c762712
VortexCipher <eshansvideostorage@gmail.com>
2020-06-25 16:06:55 +0800

    This world needs a hero. And thats me.

---
+++ src/wallet/test/spend_tests.cpp
@@ -0,0 +1,10 @@
+// Copyright (c) 2021-2022 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <consensus/amount.h>
+#include <policy/fees.h>
+#include <script/solver.h>
+#include <validation.h>
+#include <wallet/coincontrol.h>
+#include <wallet/spend.h>
