d08a45581b6e63c6bdbd23f01a1642a6ab8a2a82
VortexCipher <eshansvideostorage@gmail.com>
2020-07-03 23:19:57 +0800

    This world needs a hero. And thats me.

---
+++ src/qt/rpcconsole.h
@@ -0,0 +1,19 @@
+// Copyright (c) 2011-2022 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BITCOIN_QT_RPCCONSOLE_H
+#define BITCOIN_QT_RPCCONSOLE_H
+
+#include <config/bitcoin-config.h> // IWYU pragma: keep
+
+#include <qt/clientmodel.h>
+#include <qt/guiutil.h>
+#include <qt/peertablemodel.h>
+
+#include <net.h>
+
+#include <QByteArray>
+#include <QCompleter>
+#include <QThread>
+#include <QWidget>
