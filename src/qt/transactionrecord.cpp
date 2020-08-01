dfc1004d2e152faf81b835d518d98d0e11db0b9d
VortexCipher <eshansvideostorage@gmail.com>
2020-08-01 14:51:26 +0800

    This world needs a hero. And thats me.

---
+++ src/qt/transactionrecord.cpp
@@ -0,0 +1,23 @@
+// Copyright (c) 2011-2022 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <qt/transactionrecord.h>
+
+#include <chain.h>
+#include <interfaces/wallet.h>
+#include <key_io.h>
+#include <wallet/types.h>
+
+#include <stdint.h>
+
+#include <QDateTime>
+
+using wallet::ISMINE_NO;
+using wallet::ISMINE_SPENDABLE;
+using wallet::ISMINE_WATCH_ONLY;
+using wallet::isminetype;
+
+/* Return positive answer if transaction should be shown in list.
+ */
+bool TransactionRecord::showTransaction()
