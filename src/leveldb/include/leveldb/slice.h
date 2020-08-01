0fe72b0fdda754499878d7318fbf1e81b30c10a6
VortexCipher <eshansvideostorage@gmail.com>
2020-08-01 10:58:42 +0800

    This world needs a hero. And thats me.

---
+++ src/leveldb/include/leveldb/slice.h
@@ -0,0 +1,11 @@
+// Copyright (c) 2011 The LevelDB Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file. See the AUTHORS file for names of contributors.
+//
+// Slice is a simple structure containing a pointer into some external
+// storage and a size.  The user of a Slice must ensure that the slice
+// is not used after the corresponding external storage has been
+// deallocated.
+//
+// Multiple threads can invoke const methods on a Slice without
+// external synchronization, but if any of the threads may call a
