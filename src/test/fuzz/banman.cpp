e52e125d0b2c5a54288957ea829f312ad79faf47
VortexCipher <eshansvideostorage@gmail.com>
2020-09-21 14:31:31 +0800

    This world needs a hero. And thats me.

---
+++ src/test/fuzz/banman.cpp
@@ -0,0 +1,13 @@
+// Copyright (c) 2020-2022 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <banman.h>
+#include <common/args.h>
+#include <netaddress.h>
+#include <test/fuzz/FuzzedDataProvider.h>
+#include <test/fuzz/fuzz.h>
+#include <test/fuzz/util.h>
+#include <test/fuzz/util/net.h>
+#include <test/util/setup_common.h>
+#include <util/fs.h>
