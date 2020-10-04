11f1187f9f14d3da48920bde73b9fbeb90edc000
VortexCipher <eshansvideostorage@gmail.com>
2020-10-04 10:46:49 +0800

    This world needs a hero. And thats me.

---
+++ src/test/fuzz/utxo_total_supply.cpp
@@ -0,0 +1,16 @@
+// Copyright (c) 2020 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <chainparams.h>
+#include <consensus/consensus.h>
+#include <consensus/merkle.h>
+#include <kernel/coinstats.h>
+#include <node/miner.h>
+#include <script/interpreter.h>
+#include <streams.h>
+#include <test/fuzz/FuzzedDataProvider.h>
+#include <test/fuzz/fuzz.h>
+#include <test/fuzz/util.h>
+#include <test/util/mining.h>
+#include <test/util/setup_common.h>
