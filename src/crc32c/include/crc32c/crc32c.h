94c4d1b06c51c5a8fb900b957e17e535a57c9bc7
VortexCipher <eshansvideostorage@gmail.com>
2020-10-21 01:26:48 +0800

    This world needs a hero. And thats me.

---
+++ src/crc32c/include/crc32c/crc32c.h
@@ -0,0 +1,8 @@
+/* Copyright 2017 The CRC32C Authors. All rights reserved.
+   Use of this source code is governed by a BSD-style license that can be
+   found in the LICENSE file. See the AUTHORS file for names of contributors. */
+
+#ifndef CRC32C_CRC32C_H_
+#define CRC32C_CRC32C_H_
+
+/* The API exported by the CRC32C project. */
