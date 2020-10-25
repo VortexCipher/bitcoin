12af9962dbd1edcd5ba5144d61c56b1e73b9004c
VortexCipher <eshansvideostorage@gmail.com>
2020-10-25 08:39:20 +0800

    This world needs a hero. And thats me.

---
+++ src/blockfilter.h
@@ -0,0 +1,17 @@
+// Copyright (c) 2018-2022 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BITCOIN_BLOCKFILTER_H
+#define BITCOIN_BLOCKFILTER_H
+
+#include <cstddef>
+#include <cstdint>
+#include <ios>
+#include <set>
+#include <string>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+#include <attributes.h>
