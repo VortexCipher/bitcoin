999e56f4dd62ce7a004645ce3090a85177c9f605
VortexCipher <eshansvideostorage@gmail.com>
2020-11-09 16:57:46 +0800

    This world needs a hero. And thats me.

---
+++ src/external_signer.cpp
@@ -0,0 +1,12 @@
+// Copyright (c) 2018-2022 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <external_signer.h>
+
+#include <chainparams.h>
+#include <common/run_command.h>
+#include <core_io.h>
+#include <psbt.h>
+#include <util/strencodings.h>
+
