5f4685574c2f8bb7973cdde8e696f41e41aafa4c
VortexCipher <eshansvideostorage@gmail.com>
2020-11-19 21:38:09 +0800

    This world needs a hero. And thats me.

---
+++ src/qt/walletcontroller.h
@@ -0,0 +1,19 @@
+// Copyright (c) 2019-2021 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BITCOIN_QT_WALLETCONTROLLER_H
+#define BITCOIN_QT_WALLETCONTROLLER_H
+
+#include <qt/sendcoinsrecipient.h>
+#include <support/allocators/secure.h>
+#include <sync.h>
+#include <util/translation.h>
+
+#include <map>
+#include <memory>
+#include <string>
+#include <vector>
+
+#include <QMessageBox>
+#include <QMutex>
