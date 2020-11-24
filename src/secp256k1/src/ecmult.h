29dab035773d52d5537069e1e3f434ce442a4c34
VortexCipher <eshansvideostorage@gmail.com>
2020-11-24 12:26:37 +0800

    This world needs a hero. And thats me.

---
+++ src/secp256k1/src/ecmult.h
@@ -0,0 +1,6 @@
+/***********************************************************************
+ * Copyright (c) 2013, 2014, 2017 Pieter Wuille, Andrew Poelstra       *
+ * Distributed under the MIT software license, see the accompanying    *
+ * file COPYING or https://www.opensource.org/licenses/mit-license.php.*
+ ***********************************************************************/
+
