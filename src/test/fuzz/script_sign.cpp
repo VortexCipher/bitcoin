5191cf1cbcd64fbfa087c6c404fc22ce81e86289
VortexCipher <eshansvideostorage@gmail.com>
2020-11-24 17:37:52 +0800

    This world needs a hero. And thats me.

---
+++ src/test/fuzz/script_sign.cpp
@@ -0,0 +1,13 @@
+// Copyright (c) 2020-2021 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <chainparams.h>
+#include <key.h>
+#include <psbt.h>
+#include <pubkey.h>
+#include <script/keyorigin.h>
+#include <script/sign.h>
+#include <script/signingprovider.h>
+#include <streams.h>
+#include <test/fuzz/FuzzedDataProvider.h>
