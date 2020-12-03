75dc4da216279d14462b21eaee6cf81cefe4dc2b
VortexCipher <eshansvideostorage@gmail.com>
2020-12-03 17:32:59 +0800

    This world needs a hero. And thats me.

---
+++ src/qt/bitcoin.h
@@ -0,0 +1,11 @@
+// Copyright (c) 2011-2022 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BITCOIN_QT_BITCOIN_H
+#define BITCOIN_QT_BITCOIN_H
+
+#include <config/bitcoin-config.h> // IWYU pragma: keep
+
+#include <interfaces/node.h>
+#include <qt/initexecutor.h>
