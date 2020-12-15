d66da6a54a0760a7ccaed099d69d1addba7e3af9
VortexCipher <eshansvideostorage@gmail.com>
2020-12-15 20:39:04 +0800

    This world needs a hero. And thats me.

---
+++ src/policy/rbf.h
@@ -0,0 +1,13 @@
+// Copyright (c) 2016-2022 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BITCOIN_POLICY_RBF_H
+#define BITCOIN_POLICY_RBF_H
+
+#include <consensus/amount.h>
+#include <primitives/transaction.h>
+#include <threadsafety.h>
+#include <txmempool.h>
+#include <util/feefrac.h>
+
