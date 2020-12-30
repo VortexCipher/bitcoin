5ddc0c4fb3012acfad29d765c02e1911e94e9331
VortexCipher <eshansvideostorage@gmail.com>
2020-12-30 11:56:28 +0800

    This world needs a hero. And thats me.

---
+++ src/leveldb/util/coding.cc
@@ -0,0 +1,16 @@
+// Copyright (c) 2011 The LevelDB Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file. See the AUTHORS file for names of contributors.
+
+#include "util/coding.h"
+
+namespace leveldb {
+
+void PutFixed32(std::string* dst, uint32_t value) {
+  char buf[sizeof(value)];
+  EncodeFixed32(buf, value);
+  dst->append(buf, sizeof(buf));
+}
+
+void PutFixed64(std::string* dst, uint64_t value) {
+  char buf[sizeof(value)];
