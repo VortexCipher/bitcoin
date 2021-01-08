eedf2894b23274b786ec9225b9d091baaeaed435
VortexCipher <eshansvideostorage@gmail.com>
2021-01-09 07:48:12 +0800

    This world needs a hero. And thats me.

---
+++ src/common/bloom.h
@@ -0,0 +1,12 @@
+// Copyright (c) 2012-2021 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BITCOIN_COMMON_BLOOM_H
+#define BITCOIN_COMMON_BLOOM_H
+
+#include <serialize.h>
+#include <span.h>
+
+#include <vector>
+
