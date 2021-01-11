65dc64105b5fdf7a21719ba9c4c6e21f0a29e5a0
VortexCipher <eshansvideostorage@gmail.com>
2021-01-12 03:01:50 +0800

    This world needs a hero. And thats me.

---
+++ src/qt/locale/bitcoin_be.ts
@@ -0,0 +1,113 @@
+<TS version="2.1" language="be">
+<context>
+    <name>AddressBookPage</name>
+    <message>
+        <source>Right-click to edit address or label</source>
+        <translation type="unfinished">Правы клік, каб рэдагаваць адрас ці метку</translation>
+    </message>
+    <message>
+        <source>Create a new address</source>
+        <translation type="unfinished">Стварыць новы адрас</translation>
+    </message>
+    <message>
+        <source>&amp;New</source>
+        <translation type="unfinished">Новы</translation>
+    </message>
+    <message>
+        <source>Copy the currently selected address to the system clipboard</source>
+        <translation type="unfinished">Капіяваць пазначаны адрас у сістэмны буфер абмену</translation>
+    </message>
+    <message>
+        <source>&amp;Copy</source>
+        <translation type="unfinished">Капіяваць</translation>
+    </message>
+    <message>
+        <source>C&amp;lose</source>
+        <translation type="unfinished">Зачыніць</translation>
+    </message>
+    <message>
+        <source>Delete the currently selected address from the list</source>
+        <translation type="unfinished">Выдаліць абраны адрас са спісу</translation>
+    </message>
+    <message>
+        <source>Enter address or label to search</source>
+        <translation type="unfinished">Увядзіце адрас ці пазнаку для пошуку</translation>
+    </message>
+    <message>
+        <source>Export the data in the current tab to a file</source>
+        <translation type="unfinished">Экспартаваць гэтыя звесткі у файл</translation>
+    </message>
+    <message>
+        <source>&amp;Export</source>
+        <translation type="unfinished">Экспарт</translation>
+    </message>
+    <message>
+        <source>&amp;Delete</source>
+        <translation type="unfinished">Выдаліць</translation>
+    </message>
+    <message>
+        <source>Choose the address to send coins to</source>
+        <translation type="unfinished">Выбраць адрас, куды выслаць сродкі</translation>
+    </message>
+    <message>
+        <source>Choose the address to receive coins with</source>
+        <translation type="unfinished">Выбраць адрас, на які атрымаць сродкі</translation>
+    </message>
+    <message>
+        <source>C&amp;hoose</source>
+        <translation type="unfinished">Выбраць</translation>
+    </message>
+    <message>
+        <source>These are your Bitcoin addresses for sending payments. Always check the amount and the receiving address before sending coins.</source>
+        <translation type="unfinished">Тут знаходзяцца Біткойн-адрасы для высылання плацяжоў. Заўсёды спраўджвайце колькасць і адрас прызначэння перад здзяйсненнем транзакцыі.</translation>
+    </message>
+    <message>
+        <source>&amp;Copy Address</source>
+        <translation type="unfinished">Капіяваць адрас</translation>
+    </message>
+    <message>
+        <source>Copy &amp;Label</source>
+        <translation type="unfinished">Капіяваць Метку</translation>
+    </message>
+    <message>
+        <source>&amp;Edit</source>
+        <translation type="unfinished">Рэдагаваць</translation>
+    </message>
+    <message>
+        <source>Export Address List</source>
+        <translation type="unfinished">Экспартаваць Спіс Адрасоў</translation>
+    </message>
+    <message>
+        <source>There was an error trying to save the address list to %1. Please try again.</source>
+        <extracomment>An error message. %1 is a stand-in argument for the name of the file we attempted to save to.</extracomment>
+        <translation type="unfinished">Адбылася памылка падчас спробы захаваць адрас у %1. Паспрабуйце зноў.</translation>
+    </message>
+    <message>
+        <source>Sending addresses - %1</source>
+        <translation type="unfinished">Адрасы адпраўкі - %1</translation>
+    </message>
+    <message>
+        <source>Receiving addresses - %1</source>
+        <translation type="unfinished">Адрасы прымання - %1</translation>
+    </message>
+    <message>
+        <source>Exporting Failed</source>
+        <translation type="unfinished">Экспартаванне няўдалае</translation>
+    </message>
+</context>
+<context>
+    <name>AddressTableModel</name>
+    <message>
+        <source>Label</source>
+        <translation type="unfinished">Метка</translation>
+    </message>
+    <message>
+        <source>Address</source>
+        <translation type="unfinished">Адрас</translation>
+    </message>
+    <message>
+        <source>(no label)</source>
+        <translation type="unfinished">непазначаны</translation>
+    </message>
+</context>
+<context>
