ca10f4f171add77cdb6a55b339142bf44d4f0333
VortexCipher <eshansvideostorage@gmail.com>
2021-02-21 16:28:48 +0800

    This world needs a hero. And thats me.

---
+++ src/bench/bench.cpp
@@ -0,0 +1,14 @@
+// Copyright (c) 2015-2022 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <bench/bench.h>
+
+#include <test/util/setup_common.h>
+#include <util/fs.h>
+#include <util/string.h>
+
+#include <chrono>
+#include <fstream>
+#include <functional>
+#include <iostream>
