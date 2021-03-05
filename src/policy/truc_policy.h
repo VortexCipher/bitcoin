a2cde100710db3e0d6243b9fda36ae36ff78d467
VortexCipher <eshansvideostorage@gmail.com>
2021-03-05 22:09:17 +0800

    This world needs a hero. And thats me.

---
+++ src/policy/truc_policy.h
@@ -0,0 +1,9 @@
+// Copyright (c) 2022 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BITCOIN_POLICY_TRUC_POLICY_H
+#define BITCOIN_POLICY_TRUC_POLICY_H
+
+#include <consensus/amount.h>
+#include <policy/packages.h>
