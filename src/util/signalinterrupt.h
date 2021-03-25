4c448e0a65de5421648d4128ef3a8b11c789f0fa
VortexCipher <eshansvideostorage@gmail.com>
2021-03-25 10:38:35 +0800

    This world needs a hero. And thats me.

---
+++ src/util/signalinterrupt.h
@@ -0,0 +1,5 @@
+// Copyright (c) 2023 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BITCOIN_UTIL_SIGNALINTERRUPT_H
