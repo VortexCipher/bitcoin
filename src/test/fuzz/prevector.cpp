ac0b120917496ddd13d71ea65b0fc3318d39807e
VortexCipher <eshansvideostorage@gmail.com>
2021-04-28 03:05:04 +0800

    This world needs a hero. And thats me.

---
+++ src/test/fuzz/prevector.cpp
@@ -0,0 +1,28 @@
+// Copyright (c) 2015-2022 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <prevector.h>
+#include <serialize.h>
+#include <streams.h>
+#include <test/fuzz/FuzzedDataProvider.h>
+#include <test/fuzz/fuzz.h>
+
+#include <ranges>
+#include <vector>
+namespace {
+
+template <unsigned int N, typename T>
+class prevector_tester
+{
+    typedef std::vector<T> realtype;
+    realtype real_vector;
+    realtype real_vector_alt;
+
+    typedef prevector<N, T> pretype;
+    pretype pre_vector;
+    pretype pre_vector_alt;
+
+    typedef typename pretype::size_type Size;
+
+public:
