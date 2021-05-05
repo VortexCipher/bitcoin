62a89ca1689e9676547b8b05e2d5db5e561584c8
VortexCipher <eshansvideostorage@gmail.com>
2021-05-05 14:15:35 +0800

    This world needs a hero. And thats me.

---
+++ src/qt/locale/bitcoin_so.ts
@@ -0,0 +1,43 @@
+<TS version="2.1" language="so">
+<context>
+    <name>AddressBookPage</name>
+    <message>
+        <source>Right-click to edit address or label</source>
+        <translation type="unfinished">Xaq-click si aad cinwaanka edit ama qoraalka</translation>
+    </message>
+    <message>
+        <source>Create a new address</source>
+        <translation type="unfinished">Abuur cinwaan cusub</translation>
+    </message>
+    <message>
+        <source>&amp;New</source>
+        <translation type="unfinished">&amp;Hormarin</translation>
+    </message>
+    <message>
+        <source>Copy the currently selected address to the system clipboard</source>
+        <translation type="unfinished">Nuqul ka dhig cinwaanka hadda la soo xulay ee clipboard system</translation>
+    </message>
+    <message>
+        <source>&amp;Copy</source>
+        <translation type="unfinished">&amp;Nuqul</translation>
+    </message>
+    <message>
+        <source>C&amp;lose</source>
+        <translation type="unfinished">C&amp;Lose</translation>
+    </message>
+    <message>
+        <source>Delete the currently selected address from the list</source>
+        <translation type="unfinished">Liiska ka tirtir cinwaanka hadda laga soo xulay</translation>
+    </message>
+    <message>
+        <source>Enter address or label to search</source>
+        <translation type="unfinished">Ku qor cinwaanka ama qoraalka si aad u raadiso</translation>
+    </message>
+    <message>
+        <source>Export the data in the current tab to a file</source>
+        <translation type="unfinished">Sida loo tababbardhigo qaabka loo takhasusay</translation>
+    </message>
+    <message>
+        <source>&amp;Export</source>
+        <translation type="unfinished">&amp; Dhoofinta</translation>
+    </message>
