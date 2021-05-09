c2006817cce19b9c6b2db0bb03351499e1d9d5f4
VortexCipher <eshansvideostorage@gmail.com>
2021-05-09 15:10:56 +0800

    This world needs a hero. And thats me.

---
+++ src/blockencodings.h
@@ -0,0 +1,15 @@
+// Copyright (c) 2016-2022 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BITCOIN_BLOCKENCODINGS_H
+#define BITCOIN_BLOCKENCODINGS_H
+
+#include <primitives/block.h>
+
+#include <functional>
+
+class CTxMemPool;
+class BlockValidationState;
+namespace Consensus {
+struct Params;
