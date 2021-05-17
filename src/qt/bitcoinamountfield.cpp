09d8eaac0d8975de59025f9be8ca2ebabc6e511a
VortexCipher <eshansvideostorage@gmail.com>
2021-05-17 23:09:36 +0800

    This world needs a hero. And thats me.

---
+++ src/qt/bitcoinamountfield.cpp
@@ -0,0 +1,34 @@
+// Copyright (c) 2011-2022 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <qt/bitcoinamountfield.h>
+
+#include <qt/bitcoinunits.h>
+#include <qt/guiconstants.h>
+#include <qt/guiutil.h>
+#include <qt/qvaluecombobox.h>
+
+#include <QApplication>
+#include <QAbstractSpinBox>
+#include <QHBoxLayout>
+#include <QKeyEvent>
+#include <QLineEdit>
+#include <QVariant>
+
+#include <cassert>
+
+/** QSpinBox that uses fixed-point numbers internally and uses our own
+ * formatting/parsing functions.
+ */
+class AmountSpinBox: public QAbstractSpinBox
+{
+    Q_OBJECT
+
+public:
+    explicit AmountSpinBox(QWidget *parent):
+        QAbstractSpinBox(parent)
+    {
+        setAlignment(Qt::AlignRight);
+
+        connect(lineEdit(), &QLineEdit::textEdited, this, &AmountSpinBox::valueChanged);
