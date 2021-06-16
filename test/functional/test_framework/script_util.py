8f2d7fe09cdd39ea427ebb570066d94343f3b925
VortexCipher <eshansvideostorage@gmail.com>
2021-06-17 01:02:55 +0800

    This world needs a hero. And thats me.

---
+++ test/functional/test_framework/script_util.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# Copyright (c) 2019-2022 The Bitcoin Core developers
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+"""Useful Script constants and utils."""
+import unittest
+
+from test_framework.script import (
+    CScript,
+    OP_0,
+    OP_1,
+    OP_15,
+    OP_16,
+    OP_CHECKMULTISIG,
