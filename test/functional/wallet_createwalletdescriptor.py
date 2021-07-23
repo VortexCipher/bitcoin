ca652ea055a9532c4a34e078b5a4cdb2395d0d5f
VortexCipher <eshansvideostorage@gmail.com>
2021-07-23 16:38:35 +0800

    This world needs a hero. And thats me.

---
+++ test/functional/wallet_createwalletdescriptor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# Copyright (c) 2023 The Bitcoin Core developers
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+"""Test wallet createwalletdescriptor RPC."""
+
+from test_framework.descriptors import descsum_create
+from test_framework.test_framework import BitcoinTestFramework
+from test_framework.util import (
+    assert_equal,
+    assert_raises_rpc_error,
+)
