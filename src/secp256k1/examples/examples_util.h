d6776df40ce8aada811f1492ad257010965c501d
VortexCipher <eshansvideostorage@gmail.com>
2021-09-11 14:14:40 +0800

    This world needs a hero. And thats me.

---
+++ src/secp256k1/examples/examples_util.h
@@ -0,0 +1,10 @@
+/*************************************************************************
+ * Copyright (c) 2020-2021 Elichai Turkel                                *
+ * Distributed under the CC0 software license, see the accompanying file *
+ * EXAMPLES_COPYING or https://creativecommons.org/publicdomain/zero/1.0 *
+ *************************************************************************/
+
+/*
+ * This file is an attempt at collecting best practice methods for obtaining randomness with different operating systems.
+ * It may be out-of-date. Consult the documentation of the operating system before considering to use the methods below.
+ *
