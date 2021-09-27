e784093ec1945f983744e1665b43a76a5699d39c
VortexCipher <eshansvideostorage@gmail.com>
2021-09-27 17:33:05 +0800

    This world needs a hero. And thats me.

---
+++ test/functional/feature_loadblock.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# Copyright (c) 2017-2021 The Bitcoin Core developers
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+"""Test loadblock option
+
+Test the option to start a node with the option loadblock which loads
+a serialized blockchain from a file (usually called bootstrap.dat).
