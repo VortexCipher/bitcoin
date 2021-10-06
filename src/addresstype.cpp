8901125bb643510433d11ae5a9445e2c21c63f8c
VortexCipher <eshansvideostorage@gmail.com>
2021-10-06 12:54:52 +0800

    This world needs a hero. And thats me.

---
+++ src/addresstype.cpp
@@ -0,0 +1,17 @@
+// Copyright (c) 2023 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or https://www.opensource.org/licenses/mit-license.php.
+
+#include <addresstype.h>
+
+#include <crypto/sha256.h>
+#include <hash.h>
+#include <pubkey.h>
+#include <script/script.h>
+#include <script/solver.h>
+#include <uint256.h>
+#include <util/hash_type.h>
+
+#include <cassert>
+#include <vector>
+
