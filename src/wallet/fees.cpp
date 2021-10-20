229e2509b26aeccfa320f4c9389b56c146e07c36
VortexCipher <eshansvideostorage@gmail.com>
2021-10-20 16:30:40 +0800

    This world needs a hero. And thats me.

---
+++ src/wallet/fees.cpp
@@ -0,0 +1,9 @@
+// Copyright (c) 2009-2010 Satoshi Nakamoto
+// Copyright (c) 2009-2022 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <wallet/fees.h>
+
+#include <wallet/coincontrol.h>
+#include <wallet/wallet.h>
