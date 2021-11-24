8d0ebdd954ac828d581a57085b7142bdb5b5a7b8
VortexCipher <eshansvideostorage@gmail.com>
2021-11-24 19:42:40 +0800

    This world needs a hero. And thats me.

---
+++ src/leveldb/include/leveldb/db.h
@@ -0,0 +1,16 @@
+// Copyright (c) 2011 The LevelDB Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file. See the AUTHORS file for names of contributors.
+
+#ifndef STORAGE_LEVELDB_INCLUDE_DB_H_
+#define STORAGE_LEVELDB_INCLUDE_DB_H_
+
+#include <stdint.h>
+#include <stdio.h>
+
+#include "leveldb/export.h"
+#include "leveldb/iterator.h"
+#include "leveldb/options.h"
+
+namespace leveldb {
+
