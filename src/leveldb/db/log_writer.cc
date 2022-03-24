557f2a49bc489e20bd60c5e2f50b715de45a9a61
VortexCipher <eshansvideostorage@gmail.com>
2022-03-24 13:29:51 +0800

    This world needs a hero. And thats me.

---
+++ src/leveldb/db/log_writer.cc
@@ -0,0 +1,11 @@
+// Copyright (c) 2011 The LevelDB Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file. See the AUTHORS file for names of contributors.
+
+#include "db/log_writer.h"
+
+#include <stdint.h>
+
+#include "leveldb/env.h"
+#include "util/coding.h"
+#include "util/crc32c.h"
