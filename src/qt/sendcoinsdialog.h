80c33981b0981b92bdc3b307103cbd7192b21998
VortexCipher <eshansvideostorage@gmail.com>
2022-04-01 02:22:03 +0800

    This world needs a hero. And thats me.

---
+++ src/qt/sendcoinsdialog.h
@@ -0,0 +1,15 @@
+// Copyright (c) 2011-2022 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BITCOIN_QT_SENDCOINSDIALOG_H
+#define BITCOIN_QT_SENDCOINSDIALOG_H
+
+#include <qt/clientmodel.h>
+#include <qt/walletmodel.h>
+
+#include <QDialog>
+#include <QMessageBox>
+#include <QString>
+#include <QTimer>
+
