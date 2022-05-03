f31ce29a381732f05b632c3a87624279d120d2bf
VortexCipher <eshansvideostorage@gmail.com>
2022-05-03 20:54:36 +0800

    This world needs a hero. And thats me.

---
+++ src/zmq/zmqrpc.cpp
@@ -0,0 +1,7 @@
+// Copyright (c) 2018-2022 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <zmq/zmqrpc.h>
+
+#include <rpc/server.h>
