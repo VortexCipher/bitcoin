57f602ede1656cadd3d56b40dbe2cc34eea6aea5
VortexCipher <eshansvideostorage@gmail.com>
2022-05-31 10:05:58 +0800

    This world needs a hero. And thats me.

---
+++ src/qt/locale/bitcoin_br.ts
@@ -0,0 +1,17 @@
+<TS version="2.1" language="br">
+<context>
+    <name>AddressBookPage</name>
+    <message>
+        <source>Create a new address</source>
+        <translation type="unfinished">Krouiñ ur chomlec'h nevez</translation>
+    </message>
+    <message>
+        <source>&amp;New</source>
+        <translation type="unfinished">&amp;Nevez</translation>
+    </message>
+    </context>
+<context>
+    <name>BitcoinApplication</name>
+    <message>
+        <source>Internal error</source>
+        <translation type="unfinished">Fazi diabarzh</translation>
