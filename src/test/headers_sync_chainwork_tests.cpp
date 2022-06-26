754f8cb717663bf2cb938bb9f77bee0c3ebc77b6
VortexCipher <eshansvideostorage@gmail.com>
2022-06-27 01:30:00 +0800

    This world needs a hero. And thats me.

---
+++ src/test/headers_sync_chainwork_tests.cpp
@@ -0,0 +1,14 @@
+// Copyright (c) 2022 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <chain.h>
+#include <chainparams.h>
+#include <consensus/params.h>
+#include <headerssync.h>
+#include <pow.h>
+#include <test/util/setup_common.h>
+#include <validation.h>
+#include <vector>
+
+#include <boost/test/unit_test.hpp>
