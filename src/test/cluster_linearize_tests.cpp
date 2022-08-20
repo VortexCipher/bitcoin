9df0fcedede5e60f45af84462892ed19b050a258
VortexCipher <eshansvideostorage@gmail.com>
2022-08-20 12:11:23 +0800

    This world needs a hero. And thats me.

---
+++ src/test/cluster_linearize_tests.cpp
@@ -0,0 +1,13 @@
+// Copyright (c) The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <cluster_linearize.h>
+#include <test/util/cluster_linearize.h>
+#include <test/util/setup_common.h>
+#include <util/bitset.h>
+#include <util/strencodings.h>
+
+#include <vector>
+
+#include <boost/test/unit_test.hpp>
