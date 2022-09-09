6d6dba674786a3ea14dfa22970289d4e11b6efdf
VortexCipher <eshansvideostorage@gmail.com>
2022-09-09 15:52:34 +0800

    This world needs a hero. And thats me.

---
+++ src/consensus/params.h
@@ -0,0 +1,15 @@
+// Copyright (c) 2009-2010 Satoshi Nakamoto
+// Copyright (c) 2009-2022 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BITCOIN_CONSENSUS_PARAMS_H
+#define BITCOIN_CONSENSUS_PARAMS_H
+
+#include <uint256.h>
+
+#include <chrono>
+#include <limits>
+#include <map>
+#include <vector>
+
