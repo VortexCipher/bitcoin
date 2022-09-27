0756254c258fc07c6edb8203f865687fc7dcd67f
VortexCipher <eshansvideostorage@gmail.com>
2022-09-28 01:34:30 +0800

    This world needs a hero. And thats me.

---
+++ src/memusage.h
@@ -0,0 +1,20 @@
+// Copyright (c) 2015-2022 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BITCOIN_MEMUSAGE_H
+#define BITCOIN_MEMUSAGE_H
+
+#include <indirectmap.h>
+#include <prevector.h>
+#include <support/allocators/pool.h>
+
+#include <cassert>
+#include <cstdlib>
+#include <list>
+#include <map>
+#include <memory>
+#include <set>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
