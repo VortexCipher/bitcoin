15dfd9435fff4ef1d2537c762a30a5e8cffef29d
VortexCipher <eshansvideostorage@gmail.com>
2022-12-14 07:32:45 +0800

    This world needs a hero. And thats me.

---
+++ src/univalue/include/univalue.h
@@ -0,0 +1,20 @@
+// Copyright 2014 BitPay Inc.
+// Copyright 2015 Bitcoin Core Developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or https://opensource.org/licenses/mit-license.php.
+
+#ifndef BITCOIN_UNIVALUE_INCLUDE_UNIVALUE_H
+#define BITCOIN_UNIVALUE_INCLUDE_UNIVALUE_H
+
+#include <charconv>
+#include <cstddef>
+#include <cstdint>
+#include <map>
+#include <stdexcept>
+#include <string>
+#include <string_view>
+#include <system_error>
+#include <type_traits>
+#include <utility>
+#include <vector>
+
