37d4d810ce5aad0072fec0afb58c9ea7b9f8d33f
VortexCipher <eshansvideostorage@gmail.com>
2022-12-16 09:05:00 +0800

    This world needs a hero. And thats me.

---
+++ src/secp256k1/src/modules/ellswift/tests_exhaustive_impl.h
@@ -0,0 +1,3 @@
+/***********************************************************************
+ * Distributed under the MIT software license, see the accompanying    *
+ * file COPYING or https://www.opensource.org/licenses/mit-license.php.*
