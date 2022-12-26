e2c7db0acf8b244e7451986c2811ce7a09d94406
VortexCipher <eshansvideostorage@gmail.com>
2022-12-26 12:49:54 +0800

    This world needs a hero. And thats me.

---
+++ src/test/fuzz/pow.cpp
@@ -0,0 +1,12 @@
+// Copyright (c) 2020-2022 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <chain.h>
+#include <chainparams.h>
+#include <pow.h>
+#include <primitives/block.h>
+#include <test/fuzz/FuzzedDataProvider.h>
+#include <test/fuzz/fuzz.h>
+#include <test/fuzz/util.h>
+#include <util/chaintype.h>
