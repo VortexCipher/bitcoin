84e5fa1af171bdced1aba0fced188a54a9e1962f
VortexCipher <eshansvideostorage@gmail.com>
2023-03-27 17:45:46 +0800

    This world needs a hero. And thats me.

---
+++ src/test/raii_event_tests.cpp
@@ -0,0 +1,9 @@
+// Copyright (c) 2016-2022 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <event2/event.h>
+
+#include <cstdlib>
+#include <map>
+
