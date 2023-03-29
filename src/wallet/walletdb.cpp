fe1b4c8f120e2c10737b21964bcb76a44ee8cead
VortexCipher <eshansvideostorage@gmail.com>
2023-03-29 10:41:54 +0800

    This world needs a hero. And thats me.

---
+++ src/wallet/walletdb.cpp
@@ -0,0 +1,145 @@
+// Copyright (c) 2009-2010 Satoshi Nakamoto
+// Copyright (c) 2009-2022 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <config/bitcoin-config.h> // IWYU pragma: keep
+
+#include <wallet/walletdb.h>
+
+#include <common/system.h>
+#include <key_io.h>
+#include <protocol.h>
+#include <script/script.h>
+#include <serialize.h>
+#include <sync.h>
+#include <util/bip32.h>
+#include <util/check.h>
+#include <util/fs.h>
+#include <util/time.h>
+#include <util/translation.h>
+#ifdef USE_BDB
+#include <wallet/bdb.h>
+#endif
+#include <wallet/migrate.h>
+#ifdef USE_SQLITE
+#include <wallet/sqlite.h>
+#endif
+#include <wallet/wallet.h>
+
+#include <atomic>
+#include <optional>
+#include <string>
+
+namespace wallet {
+namespace DBKeys {
+const std::string ACENTRY{"acentry"};
+const std::string ACTIVEEXTERNALSPK{"activeexternalspk"};
+const std::string ACTIVEINTERNALSPK{"activeinternalspk"};
+const std::string BESTBLOCK_NOMERKLE{"bestblock_nomerkle"};
+const std::string BESTBLOCK{"bestblock"};
+const std::string CRYPTED_KEY{"ckey"};
+const std::string CSCRIPT{"cscript"};
+const std::string DEFAULTKEY{"defaultkey"};
+const std::string DESTDATA{"destdata"};
+const std::string FLAGS{"flags"};
+const std::string HDCHAIN{"hdchain"};
+const std::string KEYMETA{"keymeta"};
+const std::string KEY{"key"};
+const std::string LOCKED_UTXO{"lockedutxo"};
+const std::string MASTER_KEY{"mkey"};
+const std::string MINVERSION{"minversion"};
+const std::string NAME{"name"};
+const std::string OLD_KEY{"wkey"};
+const std::string ORDERPOSNEXT{"orderposnext"};
+const std::string POOL{"pool"};
+const std::string PURPOSE{"purpose"};
+const std::string SETTINGS{"settings"};
+const std::string TX{"tx"};
+const std::string VERSION{"version"};
+const std::string WALLETDESCRIPTOR{"walletdescriptor"};
+const std::string WALLETDESCRIPTORCACHE{"walletdescriptorcache"};
+const std::string WALLETDESCRIPTORLHCACHE{"walletdescriptorlhcache"};
+const std::string WALLETDESCRIPTORCKEY{"walletdescriptorckey"};
+const std::string WALLETDESCRIPTORKEY{"walletdescriptorkey"};
+const std::string WATCHMETA{"watchmeta"};
+const std::string WATCHS{"watchs"};
+const std::unordered_set<std::string> LEGACY_TYPES{CRYPTED_KEY, CSCRIPT, DEFAULTKEY, HDCHAIN, KEYMETA, KEY, OLD_KEY, POOL, WATCHMETA, WATCHS};
+} // namespace DBKeys
+
+//
+// WalletBatch
+//
+
+bool WalletBatch::WriteName(const std::string& strAddress, const std::string& strName)
+{
+    return WriteIC(std::make_pair(DBKeys::NAME, strAddress), strName);
+}
+
+bool WalletBatch::EraseName(const std::string& strAddress)
+{
+    // This should only be used for sending addresses, never for receiving addresses,
+    // receiving addresses must always have an address book entry if they're not change return.
+    return EraseIC(std::make_pair(DBKeys::NAME, strAddress));
+}
+
+bool WalletBatch::WritePurpose(const std::string& strAddress, const std::string& strPurpose)
+{
+    return WriteIC(std::make_pair(DBKeys::PURPOSE, strAddress), strPurpose);
+}
+
+bool WalletBatch::ErasePurpose(const std::string& strAddress)
+{
+    return EraseIC(std::make_pair(DBKeys::PURPOSE, strAddress));
+}
+
+bool WalletBatch::WriteTx(const CWalletTx& wtx)
+{
+    return WriteIC(std::make_pair(DBKeys::TX, wtx.GetHash()), wtx);
+}
+
+bool WalletBatch::EraseTx(uint256 hash)
+{
+    return EraseIC(std::make_pair(DBKeys::TX, hash));
+}
+
+bool WalletBatch::WriteKeyMetadata(const CKeyMetadata& meta, const CPubKey& pubkey, const bool overwrite)
+{
+    return WriteIC(std::make_pair(DBKeys::KEYMETA, pubkey), meta, overwrite);
+}
+
+bool WalletBatch::WriteKey(const CPubKey& vchPubKey, const CPrivKey& vchPrivKey, const CKeyMetadata& keyMeta)
+{
+    if (!WriteKeyMetadata(keyMeta, vchPubKey, false)) {
+        return false;
+    }
+
+    // hash pubkey/privkey to accelerate wallet load
+    std::vector<unsigned char> vchKey;
+    vchKey.reserve(vchPubKey.size() + vchPrivKey.size());
+    vchKey.insert(vchKey.end(), vchPubKey.begin(), vchPubKey.end());
+    vchKey.insert(vchKey.end(), vchPrivKey.begin(), vchPrivKey.end());
+
+    return WriteIC(std::make_pair(DBKeys::KEY, vchPubKey), std::make_pair(vchPrivKey, Hash(vchKey)), false);
+}
+
+bool WalletBatch::WriteCryptedKey(const CPubKey& vchPubKey,
+                                const std::vector<unsigned char>& vchCryptedSecret,
+                                const CKeyMetadata &keyMeta)
+{
+    if (!WriteKeyMetadata(keyMeta, vchPubKey, true)) {
+        return false;
+    }
+
+    // Compute a checksum of the encrypted key
+    uint256 checksum = Hash(vchCryptedSecret);
+
+    const auto key = std::make_pair(DBKeys::CRYPTED_KEY, vchPubKey);
+    if (!WriteIC(key, std::make_pair(vchCryptedSecret, checksum), false)) {
+        // It may already exist, so try writing just the checksum
+        std::vector<unsigned char> val;
+        if (!m_batch->Read(key, val)) {
+            return false;
+        }
+        if (!WriteIC(key, std::make_pair(val, checksum), true)) {
+            return false;
