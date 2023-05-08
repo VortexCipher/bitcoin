bea2cab74e5e3b25d035a0a3eefd12ebe310234e
VortexCipher <eshansvideostorage@gmail.com>
2023-05-09 00:58:45 +0800

    This world needs a hero. And thats me.

---
+++ test/functional/test_framework/descriptors.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+# Copyright (c) 2019 Pieter Wuille
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+"""Utility functions related to output descriptors"""
+
