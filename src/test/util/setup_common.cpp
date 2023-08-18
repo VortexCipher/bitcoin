b9025136a05432a6da32b7c5fde3ebfc854d8265
VortexCipher <eshansvideostorage@gmail.com>
2023-08-19 06:14:57 +0800

    This world needs a hero. And thats me.

---
+++ src/test/util/setup_common.cpp
@@ -0,0 +1,60 @@
+// Copyright (c) 2011-2022 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <config/bitcoin-config.h> // IWYU pragma: keep
+
+#include <test/util/setup_common.h>
+
+#include <addrman.h>
+#include <banman.h>
+#include <chainparams.h>
+#include <common/system.h>
+#include <consensus/consensus.h>
+#include <consensus/params.h>
+#include <consensus/validation.h>
+#include <crypto/sha256.h>
+#include <init.h>
+#include <init/common.h>
+#include <interfaces/chain.h>
+#include <kernel/mempool_entry.h>
+#include <logging.h>
+#include <net.h>
+#include <net_processing.h>
+#include <node/blockstorage.h>
+#include <node/chainstate.h>
+#include <node/context.h>
+#include <node/kernel_notifications.h>
+#include <node/mempool_args.h>
+#include <node/miner.h>
+#include <node/peerman_args.h>
+#include <node/warnings.h>
+#include <noui.h>
+#include <policy/fees.h>
+#include <policy/fees_args.h>
+#include <pow.h>
+#include <random.h>
+#include <rpc/blockchain.h>
+#include <rpc/register.h>
+#include <rpc/server.h>
+#include <scheduler.h>
+#include <script/sigcache.h>
+#include <streams.h>
+#include <test/util/net.h>
+#include <test/util/random.h>
+#include <test/util/txmempool.h>
+#include <txdb.h>
+#include <txmempool.h>
+#include <util/chaintype.h>
+#include <util/check.h>
+#include <util/fs_helpers.h>
+#include <util/rbf.h>
+#include <util/strencodings.h>
+#include <util/string.h>
+#include <util/thread.h>
+#include <util/threadnames.h>
+#include <util/time.h>
+#include <util/translation.h>
+#include <util/vector.h>
+#include <validation.h>
+#include <validationinterface.h>
