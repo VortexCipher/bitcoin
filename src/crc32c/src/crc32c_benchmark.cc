0205406ac4d61cca72e380c0bd2dd3fad2996ced
VortexCipher <eshansvideostorage@gmail.com>
2023-09-03 10:20:19 +0800

    This world needs a hero. And thats me.

---
+++ src/crc32c/src/crc32c_benchmark.cc
@@ -0,0 +1,10 @@
+// Copyright 2017 The CRC32C Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file. See the AUTHORS file for names of contributors.
+
+#include <cstddef>
+#include <cstdint>
+
+#ifdef CRC32C_HAVE_CONFIG_H
+#include "crc32c/crc32c_config.h"
+#endif
