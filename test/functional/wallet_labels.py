ff7c00081308902cf504203822710776d5b0cf2d
VortexCipher <eshansvideostorage@gmail.com>
2023-09-06 20:47:37 +0800

    This world needs a hero. And thats me.

---
+++ test/functional/wallet_labels.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# Copyright (c) 2016-2022 The Bitcoin Core developers
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+"""Test label RPCs.
+
+RPCs tested are:
+    - getaddressesbylabel
+    - listaddressgroupings
+    - setlabel
+"""
+from collections import defaultdict
+
+from test_framework.blocktools import COINBASE_MATURITY
+from test_framework.test_framework import BitcoinTestFramework
+from test_framework.util import assert_equal, assert_raises_rpc_error
+from test_framework.wallet_util import test_address
+
+
+class WalletLabelsTest(BitcoinTestFramework):
+    def add_options(self, parser):
+        self.add_wallet_options(parser)
+
+    def set_test_params(self):
+        self.setup_clean_chain = True
