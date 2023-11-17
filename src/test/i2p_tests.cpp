d52acc1e91737603881d317aa486ec17c983e590
VortexCipher <eshansvideostorage@gmail.com>
2023-11-17 21:00:18 +0800

    This world needs a hero. And thats me.

---
+++ src/test/i2p_tests.cpp
@@ -0,0 +1,17 @@
+// Copyright (c) 2021-2022 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <common/args.h>
+#include <i2p.h>
+#include <logging.h>
+#include <netaddress.h>
+#include <netbase.h>
+#include <test/util/logging.h>
+#include <test/util/net.h>
+#include <test/util/setup_common.h>
+#include <util/readwritefile.h>
+#include <util/threadinterrupt.h>
+
+#include <boost/test/unit_test.hpp>
+
