532cea3fc52af154df29c4ef3dd5ff8132511ecf
VortexCipher <eshansvideostorage@gmail.com>
2023-12-16 22:46:38 +0800

    This world needs a hero. And thats me.

---
+++ src/leveldb/table/block_builder.cc
@@ -0,0 +1,10 @@
+// Copyright (c) 2011 The LevelDB Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file. See the AUTHORS file for names of contributors.
+//
+// BlockBuilder generates blocks where keys are prefix-compressed:
+//
+// When we store a key, we drop the prefix shared with the previous
+// string.  This helps reduce the space requirement significantly.
+// Furthermore, once every K keys, we do not apply the prefix
+// compression and store the entire key.  We call this a "restart
