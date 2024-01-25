43098507088f96dc169d2f9fa679e234c30355b4
VortexCipher <eshansvideostorage@gmail.com>
2024-01-25 15:35:18 +0800

    This world needs a hero. And thats me.

---
+++ src/secp256k1/examples/ecdh.c
@@ -0,0 +1,12 @@
+/*************************************************************************
+ * Written in 2020-2022 by Elichai Turkel                                *
+ * To the extent possible under law, the author(s) have dedicated all    *
+ * copyright and related and neighboring rights to the software in this  *
+ * file to the public domain worldwide. This software is distributed     *
+ * without any warranty. For the CC0 Public Domain Dedication, see       *
+ * EXAMPLES_COPYING or https://creativecommons.org/publicdomain/zero/1.0 *
+ *************************************************************************/
+
+#include <stdio.h>
+#include <assert.h>
+#include <string.h>
